b0b8c13963bb656554a651a4471041dc07d41952
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:14:38 +0200

    Use norm helpers in VVX feature

--- rt/poll/feature-vvx.js
+++ rt/poll/feature-vvx.js
@@ -1,12 +1,11 @@
-// poll feature-vvx 1.0.1
+// poll feature-vvx 1.1.0
 var IP_VVX = "192.168.77.40";
-var VVX_MAX_W = 30;
 
 function readVvx(ctx, cb) {
   httpGetStatus(IP_VVX, function (js) {
     var x = js ? parseSwitch0(js) : null;
     ctx.vvx.on = x ? x.on : 0;
-    ctx.vvx.w = x ? clipW(x.w, VVX_MAX_W) : 0;
+    ctx.vvx.w = x ? normW(x.w) : 0;
     cb();
   });
 }
