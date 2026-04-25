6a53579d8573ec34c61035181264be0af96395ca
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:13:15 +0200

    Use norm helpers in heat feature

--- rt/poll/feature-heat.js
+++ rt/poll/feature-heat.js
@@ -1,13 +1,12 @@
-// poll feature-heat 1.0.1
+// poll feature-heat 1.1.0
 var IP_HEAT = "192.168.77.12";
-var HEAT_MAX_W = 50;
 
 function readHeat(ctx, cb) {
   httpGetStatus(IP_HEAT, function (js) {
     var x = js ? parseLight0(js) : null;
     ctx.heat.on = x ? x.on : 0;
-    ctx.heat.pct = x ? clipPct(x.pct) : 0;
-    ctx.heat.w = x ? clipW(x.w, HEAT_MAX_W) : 0;
+    ctx.heat.pct = x ? normPct(x.pct) : 0;
+    ctx.heat.w = x ? normW(x.w) : 0;
     cb();
   });
 }
