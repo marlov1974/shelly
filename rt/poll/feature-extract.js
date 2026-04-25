4904760e1ddf420f97a280e97bdb1be7a9efacb5
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:17:26 +0200

    Clean extract feature for telemetry-only poll

--- rt/poll/feature-extract.js
+++ rt/poll/feature-extract.js
@@ -1,4 +1,4 @@
-// poll feature-extract 1.0.0
+// poll feature-extract 3.0.0
 function parseExtractUni(js) {
   var vm = comp(js, "voltmeter:" + VM_DP_ID);
   var inRpm = comp(js, "input:" + INPUT_RPM_ID);
@@ -11,32 +11,18 @@ function parseExtractUni(js) {
   };
 }
 
-function deriveExtract(ctx) {
+function deriveExtractTelemetry(ctx) {
   ctx.extract.pa = clipPa(ctx.extract.pa);
   ctx.extract.rpm = clipFanRpm(ctx.extract.rpm);
   ctx.extract.ls = clipLs(paToLsExtract(ctx.extract.pa));
   ctx.extract.temp_to_house = clipTemp(ctx.extract.temp_to_house);
   ctx.extract.temp_brine = clipTemp(ctx.extract.temp_brine);
   ctx.extract.temp_hotwater = clipTemp(ctx.extract.temp_hotwater);
+  ctx.extract.fan_pct = clipPct(ctx.extract.fan_pct);
   ctx.extract.fan_w = clipW(ctx.extract.fan_w, 180);
-  ctx.extract.fan_run = b(
-    ctx.extract.fan_on &&
-    ctx.extract.fan_pct > 10 &&
-    ctx.extract.rpm > FAN_RPM_RUN_MIN &&
-    ctx.extract.pa >= FAN_DP_RUN_MIN_PA
-  );
 }
 
 function readExtract(ctx, cb) {
-  var left = 2;
-  function done() {
-    left--;
-    if (left <= 0) {
-      deriveExtract(ctx);
-      cb();
-    }
-  }
-
   httpGetStatus(IP_EXTRACT_UNI, function (js) {
     var x = js ? parseExtractUni(js) : null;
     ctx.extract.pa = x ? x.pa : 0;
@@ -44,14 +30,14 @@ function readExtract(ctx, cb) {
     ctx.extract.temp_to_house = x ? x.temp_to_house : 0;
     ctx.extract.temp_brine = x ? x.temp_brine : 0;
     ctx.extract.temp_hotwater = x ? x.temp_hotwater : 0;
-    done();
-  });
 
-  httpGetStatus(IP_EXTRACT_FAN, function (js) {
-    var x = js ? parseLight0(js) : null;
-    ctx.extract.fan_on = x ? x.on : 0;
-    ctx.extract.fan_pct = x ? x.pct : 0;
-    ctx.extract.fan_w = x ? x.w : 0;
-    done();
+    httpGetStatus(IP_EXTRACT_FAN, function (js2) {
+      var y = js2 ? parseLight0(js2) : null;
+      ctx.extract.fan_on = y ? y.on : 0;
+      ctx.extract.fan_pct = y ? y.pct : 0;
+      ctx.extract.fan_w = y ? y.w : 0;
+      deriveExtractTelemetry(ctx);
+      cb();
+    });
   });
 }
