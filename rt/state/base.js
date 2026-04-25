231aa291ffa8fd468cc92f2065ceceb2243e1218
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:43:27 +0200

    Move state numeric helpers out of base

--- rt/state/base.js
+++ rt/state/base.js
@@ -1,4 +1,4 @@
-// state base 1.1.0
+// state base 1.1.1
 var SCRIPT_NAME = "state";
 
 var KEY_TEL_M = "ftx.tel.m";
@@ -14,58 +14,4 @@ var FAN_SPEED_AVG_ID = 203;
 
 function log(s) {
   print(String(SCRIPT_NAME) + " " + s);
-}
-
-function n(v, d) {
-  var x = Number(v);
-  return (x === x) ? x : d;
-}
-
-function i(v) {
-  var x = Number(v);
-  if (x !== x) return 0;
-  return Math.floor(x + 0.5);
-}
-
-function d1(v) {
-  var x = Number(v);
-  if (x !== x) return 0;
-  return Math.round(x * 10) / 10;
-}
-
-function clip(v, lo, hi) {
-  var x = Number(v);
-  if (x !== x) x = 0;
-  if (x < lo) x = lo;
-  if (x > hi) x = hi;
-  return x;
-}
-
-function b(v) {
-  return v ? 1 : 0;
-}
-
-function abs(x) {
-  return x < 0 ? -x : x;
-}
-
-function sget(o, a, d) {
-  if (o && typeof o[a] === "number") return o[a];
-  return d;
-}
-
-function on(o) {
-  return o && o.on ? 1 : 0;
-}
-
-function pct(o) {
-  return sget(o, "pct", 0);
-}
-
-function w(o) {
-  return sget(o, "w", 0);
-}
-
-function clipPct(v) {
-  return i(clip(v, 0, 100));
 }
