a5e1e0b4029433b11e901fba8327bae8f22ae98f
marlov1974 <marlov74@hotmail.com>
2026-04-26 00:10:24 +0200

    Make extract run feature apply to state context

--- rt/state/feature-run-extract.js
+++ rt/state/feature-run-extract.js
@@ -1,17 +1,21 @@
-// state feature-run-extract 1.1.0
+// state feature-run-extract 1.2.0
 var EXTRACT_RUN_PCT_MIN = 10;
 var EXTRACT_RUN_RPM_MIN = 250;
 var EXTRACT_RUN_PA_MIN = 5;
 
-function calcExtractRun(telM, telAct) {
-  var ext = telAct && telAct.ext ? telAct.ext : {};
-  var rpm = telM && telM.rpm ? telM.rpm : {};
-  var pa = telM && telM.pa ? telM.pa : {};
+function applyExtractRun(ctx, cb) {
+  var telM = ctx.telM || {};
+  var telAct = ctx.telAct || {};
+  var ext = telAct.ext || {};
+  var rpm = telM.rpm || {};
+  var pa = telM.pa || {};
 
-  return b(
+  ctx.run.ext = b(
     on(ext) &&
     pct(ext) > EXTRACT_RUN_PCT_MIN &&
     sget(rpm, "ext", 0) > EXTRACT_RUN_RPM_MIN &&
     sget(pa, "ext", 0) >= EXTRACT_RUN_PA_MIN
   );
+
+  cb();
 }
