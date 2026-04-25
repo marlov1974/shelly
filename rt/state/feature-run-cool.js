8740e06e8ca31a59712309998fd5a100ef03026a
marlov1974 <marlov74@hotmail.com>
2026-04-26 00:11:57 +0200

    Make cool run feature apply to state context

--- rt/state/feature-run-cool.js
+++ rt/state/feature-run-cool.js
@@ -1,14 +1,18 @@
-// state feature-run-cool 1.1.0
+// state feature-run-cool 1.2.0
 var COOL_RUN_PCT_MIN = 0;
 var COOL_RUN_DT_MIN_C = 0.5;
 
-function calcCoolRun(telM, telAct) {
-  var cool = telAct && telAct.cool ? telAct.cool : {};
-  var t = telM && telM.t ? telM.t : {};
+function applyCoolRun(ctx, cb) {
+  var telM = ctx.telM || {};
+  var telAct = ctx.telAct || {};
+  var cool = telAct.cool || {};
+  var t = telM.t || {};
 
-  return b(
+  ctx.run.cool = b(
     on(cool) &&
     pct(cool) > COOL_RUN_PCT_MIN &&
     (sget(t, "post_vvx", 0) - sget(t, "to_house", 0)) >= COOL_RUN_DT_MIN_C
   );
+
+  cb();
 }
