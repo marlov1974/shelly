cb0240d8b45378b8c9d62f4c5d946e68cdf3010a
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:50:12 +0200

    Move heat constants into heat feature

--- rt/poll/feature-heat.js
+++ rt/poll/feature-heat.js
@@ -1,10 +1,13 @@
-// poll feature-heat 1.0.0
+// poll feature-heat 1.0.1
+var IP_HEAT = "192.168.77.12";
+var HEAT_MAX_W = 50;
+
 function readHeat(ctx, cb) {
   httpGetStatus(IP_HEAT, function (js) {
     var x = js ? parseLight0(js) : null;
     ctx.heat.on = x ? x.on : 0;
     ctx.heat.pct = x ? clipPct(x.pct) : 0;
-    ctx.heat.w = x ? clipW(x.w, 50) : 0;
+    ctx.heat.w = x ? clipW(x.w, HEAT_MAX_W) : 0;
     cb();
   });
 }
