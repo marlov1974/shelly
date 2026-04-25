872f3a0a17a91ee274c07f61056a76690daa0d46
marlov1974 <marlov74@hotmail.com>
2026-04-25 20:51:18 +0200

    Add poll debug checkpoints

--- rt/poll/main.js
+++ rt/poll/main.js
@@ -1,22 +1,50 @@
-// poll main 2.0.0
+// poll main 2.0.1-debug
+function pollDbg(s, cb) {
+  Shelly.call("Text.Set", { id: POLL_STATUS_TEXT_ID, value: String(s || "") }, function () {
+    if (cb) cb();
+  });
+}
+
 function runPoll() {
   var ctx = createPollCtx();
   log("BOT");
 
-  readSupply(ctx, function () {
-    readExtract(ctx, function () {
-      readProcess(ctx, function () {
-        readActuators(ctx, function () {
-          readVvxEffHist(ctx, function () {
-            deriveCrossFeatures(ctx, function () {
-              writeTelemetryM(ctx, function () {
-                writeTelemetryAct(ctx, function () {
-                  writeVvxEffRawHist(ctx, function () {
-                    writeTotalPower(ctx, function () {
-                      writeVvxEfficiency(ctx, function () {
-                        writeFanSpeedAvg(ctx, function () {
-                          writePollStatus(ctx, function () {
-                            log("DON");
+  pollDbg("P D0 BOT", function () {
+    pollDbg("P D1 SUP", function () {
+      readSupply(ctx, function () {
+        pollDbg("P D2 EXT", function () {
+          readExtract(ctx, function () {
+            pollDbg("P D3 PROC", function () {
+              readProcess(ctx, function () {
+                pollDbg("P D4 ACT", function () {
+                  readActuators(ctx, function () {
+                    pollDbg("P D5 HIST", function () {
+                      readVvxEffHist(ctx, function () {
+                        pollDbg("P D6 DER", function () {
+                          deriveCrossFeatures(ctx, function () {
+                            pollDbg("P D7 WTM", function () {
+                              writeTelemetryM(ctx, function () {
+                                pollDbg("P D8 WTA", function () {
+                                  writeTelemetryAct(ctx, function () {
+                                    pollDbg("P D9 WH", function () {
+                                      writeVvxEffRawHist(ctx, function () {
+                                        pollDbg("P D10 NUM", function () {
+                                          writeTotalPower(ctx, function () {
+                                            writeVvxEfficiency(ctx, function () {
+                                              writeFanSpeedAvg(ctx, function () {
+                                                writePollStatus(ctx, function () {
+                                                  log("DON");
+                                                });
+                                              });
+                                            });
+                                          });
+                                        });
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
                           });
                         });
                       });
