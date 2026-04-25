8b930149a91db4e6308af8aa1c71d614c324cc4f
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:13:49 +0200

    Use norm helpers in cool feature

--- rt/poll/feature-cool.js
+++ rt/poll/feature-cool.js
@@ -1,13 +1,12 @@
-// poll feature-cool 1.0.1
+// poll feature-cool 1.1.0
 var IP_COOL = "192.168.77.13";
-var COOL_MAX_W = 50;
 
 function readCool(ctx, cb) {
   httpGetStatus(IP_COOL, function (js) {
     var x = js ? parseLight0(js) : null;
     ctx.cool.on = x ? x.on : 0;
-    ctx.cool.pct = x ? clipPct(x.pct) : 0;
-    ctx.cool.w = x ? clipW(x.w, COOL_MAX_W) : 0;
+    ctx.cool.pct = x ? normPct(x.pct) : 0;
+    ctx.cool.w = x ? normW(x.w) : 0;
     cb();
   });
 }
