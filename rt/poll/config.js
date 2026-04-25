ee373e4d19387dd57d44d416f7ece6c6235f9b8b
marlov1974 <marlov74@hotmail.com>
2026-04-25 15:57:49 +0200

    Add poll config chunk

---
+++ rt/poll/config.js
@@ -0,0 +1,39 @@
+// poll config 1.0.0
+var IP_SUPPLY_UNI = "192.168.77.20";
+var IP_EXTRACT_UNI = "192.168.77.21";
+var IP_PROCESS_UNI = "192.168.77.22";
+var IP_SUPPLY_FAN = "192.168.77.10";
+var IP_EXTRACT_FAN = "192.168.77.11";
+var IP_HEAT = "192.168.77.12";
+var IP_COOL = "192.168.77.13";
+var IP_VVX = "192.168.77.40";
+var IP_DAMPERS = "192.168.77.30";
+
+var VM_DP_ID = 100;
+var INPUT_RPM_ID = 2;
+var TEMP_A_ID = 100;
+var TEMP_B_ID = 101;
+var TEMP_C_ID = 102;
+var RH_ID = 100;
+
+var K_SUPPLY_FAN = 11.6;
+var K_EXTRACT_FAN = 12.1;
+
+var FAN_RPM_RUN_MIN = 250;
+var FAN_DP_RUN_MIN_PA = 5;
+var VVX_RPM_RUN_MIN = 4;
+var HEAT_DT_MIN_C = 0.5;
+var COOL_DT_MIN_C = 0.5;
+
+var KEY_TEL_M = "ftx.tel.m";
+var KEY_TEL_ACT = "ftx.tel.act";
+var KEY_VVX_EFF_RAW_HIST = "ftx.vvx.eff.raw_hist";
+
+var TOTAL_POWER_ID = 201;
+var VVX_EFFICIENCY_ID = 202;
+var FAN_SPEED_AVG_ID = 203;
+
+var IDLE_POWER_W = 14;
+var DAMPERS_POWER_W = 8;
+var VVX_EFF_DEN_MIN_C = 3.0;
+
