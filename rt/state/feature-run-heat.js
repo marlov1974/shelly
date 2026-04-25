7996e0b0751055c4cd51d207c8a188341b6f4e51
marlov1974 <marlov74@hotmail.com>
2026-04-26 00:11:22 +0200

    Make heat run feature apply to state context

--- rt/state/feature-run-heat.js
+++ rt/state/feature-run-heat.js
@@ -1,14 +1,18 @@
-// state feature-run-heat 1.1.0
+// state feature-run-heat 1.2.0
 var HEAT_RUN_PCT_MIN = 0;
 var HEAT_RUN_DT_MIN_C = 0.5;
 
-function calcHeatRun(telM, telAct) {
-  var heat = telAct && telAct.heat ? telAct.heat : {};
-  var t = telM && telM.t ? telM.t : {};
+function applyHeatRun(ctx, cb) {
+  var telM = ctx.telM || {};
+  var telAct = ctx.telAct || {};
+  var heat = telAct.heat || {};
+  var t = telM.t || {};
 
-  return b(
+  ctx.run.heat = b(
     on(heat) &&
     pct(heat) > HEAT_RUN_PCT_MIN &&
     (sget(t, "to_house", 0) - sget(t, "post_vvx", 0)) >= HEAT_RUN_DT_MIN_C
   );
+
+  cb();
 }
