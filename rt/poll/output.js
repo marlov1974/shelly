b8f07c49c7ed3d9dd8624edc3325b205730d37ba
marlov1974 <marlov74@hotmail.com>
2026-04-25 20:11:59 +0200

    Refactor poll output to use context object

--- rt/poll/output.js
+++ rt/poll/output.js
@@ -1,63 +1,62 @@
-// poll output 1.0.1
-function buildTelM() {
+// poll output 2.0.0
+function buildTelM(ctx) {
   return {
     t: {
-      house: tempHouseC,
-      out: tempOutdoorC,
-      to_house: tempToHouseC,
-      post_vvx: tempPostVvxC,
-      to_outdoor: tempToOutdoorC,
-      brine: tempBrineC,
-      hotwater: tempHotwaterC
+      house: ctx.process.temp_house,
+      out: ctx.supply.temp_outdoor,
+      to_house: ctx.extract.temp_to_house,
+      post_vvx: ctx.supply.temp_post_vvx,
+      to_outdoor: ctx.supply.temp_to_outdoor,
+      brine: ctx.extract.temp_brine,
+      hotwater: ctx.extract.temp_hotwater
     },
     rpm: {
-      sup: rpmSupplyFan,
-      ext: rpmExtractFan,
-      vvx: rpmVvx
+      sup: ctx.supply.rpm,
+      ext: ctx.extract.rpm,
+      vvx: ctx.process.rpm_vvx
     },
     pa: {
-      sup: paSupply,
-      ext: paExtract
+      sup: ctx.supply.pa,
+      ext: ctx.extract.pa
     },
     ls: {
-      sup: lsSupply,
-      ext: lsExtract
+      sup: ctx.supply.ls,
+      ext: ctx.extract.ls
     },
     ppm: {
-      house: ppmHouse
+      house: ctx.process.co2_ppm
     },
     rh: {
-      house: rhHouse
+      house: ctx.process.rh_house
     }
   };
 }
 
-function buildTelAct() {
+function buildTelAct(ctx) {
   return {
-    sup: { on: actSupplyOn, pct: actSupplyPct, w: actSupplyW, run: actSupplyRun },
-    ext: { on: actExtractOn, pct: actExtractPct, w: actExtractW, run: actExtractRun },
-    vvx: { on: actVvxOn, w: actVvxW, run: actVvxRun },
-    heat: { on: actHeatOn, pct: actHeatPct, w: actHeatW, run: actHeatRun },
-    cool: { on: actCoolOn, pct: actCoolPct, w: actCoolW, run: actCoolRun },
-    dmp: { on: actDampersOn, run: actDampersRun }
+    sup: { on: ctx.supply.fan_on, pct: ctx.supply.fan_pct, w: ctx.supply.fan_w, run: ctx.supply.fan_run },
+    ext: { on: ctx.extract.fan_on, pct: ctx.extract.fan_pct, w: ctx.extract.fan_w, run: ctx.extract.fan_run },
+    vvx: { on: ctx.vvx.on, w: ctx.vvx.w, run: ctx.vvx.run },
+    heat: { on: ctx.heat.on, pct: ctx.heat.pct, w: ctx.heat.w, run: ctx.heat.run },
+    cool: { on: ctx.cool.on, pct: ctx.cool.pct, w: ctx.cool.w, run: ctx.cool.run },
+    dmp: { on: ctx.dmp.on, run: ctx.dmp.run }
   };
 }
 
-function buildVvxEffRawHist() {
-  return { r0: d1(vvxEffRaw0), r1: d1(vvxEffRaw1), r2: d1(vvxEffRaw2) };
+function buildVvxEffRawHist(ctx) {
+  return { r0: d1(ctx.hist.vvx_r0), r1: d1(ctx.hist.vvx_r1), r2: d1(ctx.hist.vvx_r2) };
 }
 
-function writeTelemetryM(cb) { kvsSet(KEY_TEL_M, buildTelM(), cb); }
-function writeTelemetryAct(cb) { kvsSet(KEY_TEL_ACT, buildTelAct(), cb); }
-function writeVvxEffRawHist(cb) { kvsSet(KEY_VVX_EFF_RAW_HIST, buildVvxEffRawHist(), cb); }
-function writeTotalPower(cb) { numberSet(TOTAL_POWER_ID, totalPowerW, cb); }
-function writeVvxEfficiency(cb) { numberSet(VVX_EFFICIENCY_ID, vvxEfficiencyPct, cb); }
-function writeFanSpeedAvg(cb) { numberSet(FAN_SPEED_AVG_ID, fanSpeedAvgPct, cb); }
+function writeTelemetryM(ctx, cb) { kvsSet(KEY_TEL_M, buildTelM(ctx), cb); }
+function writeTelemetryAct(ctx, cb) { kvsSet(KEY_TEL_ACT, buildTelAct(ctx), cb); }
+function writeVvxEffRawHist(ctx, cb) { kvsSet(KEY_VVX_EFF_RAW_HIST, buildVvxEffRawHist(ctx), cb); }
+function writeTotalPower(ctx, cb) { numberSet(TOTAL_POWER_ID, ctx.power.total_w, cb); }
+function writeVvxEfficiency(ctx, cb) { numberSet(VVX_EFFICIENCY_ID, ctx.vvx.eff_pct, cb); }
+function writeFanSpeedAvg(ctx, cb) { numberSet(FAN_SPEED_AVG_ID, ctx.fan.avg_pct, cb); }
 
-function writePollStatus(cb) {
-  var s = "P OK W=" + totalPowerW + " E=" + vvxEfficiencyPct + " F=" + fanSpeedAvgPct;
+function writePollStatus(ctx, cb) {
+  var s = "P OK W=" + ctx.power.total_w + " E=" + ctx.vvx.eff_pct + " F=" + ctx.fan.avg_pct;
   Shelly.call("Text.Set", { id: POLL_STATUS_TEXT_ID, value: s }, function () {
     if (cb) cb();
   });
 }
-
