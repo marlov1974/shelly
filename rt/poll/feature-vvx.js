3bb6f1d72898d8c74e94e4ded842b37cd3d86dff
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:51:08 +0200

    Move VVX constants into VVX feature

--- rt/poll/feature-vvx.js
+++ rt/poll/feature-vvx.js
@@ -1,9 +1,12 @@
-// poll feature-vvx 1.0.0
+// poll feature-vvx 1.0.1
+var IP_VVX = "192.168.77.40";
+var VVX_MAX_W = 30;
+
 function readVvx(ctx, cb) {
   httpGetStatus(IP_VVX, function (js) {
     var x = js ? parseSwitch0(js) : null;
     ctx.vvx.on = x ? x.on : 0;
-    ctx.vvx.w = x ? clipW(x.w, 30) : 0;
+    ctx.vvx.w = x ? clipW(x.w, VVX_MAX_W) : 0;
     cb();
   });
 }
