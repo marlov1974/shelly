0a32ec7ea204053ed866fe3130d3ac8cd2bb597a
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:51:40 +0200

    Move dampers constants into dampers feature

--- rt/poll/feature-dampers.js
+++ rt/poll/feature-dampers.js
@@ -1,4 +1,6 @@
-// poll feature-dampers 1.0.0
+// poll feature-dampers 1.0.1
+var IP_DAMPERS = "192.168.77.30";
+
 function readDampers(ctx, cb) {
   httpGetStatus(IP_DAMPERS, function (js) {
     var x = js ? parseSwitch0(js) : null;
