9104638b2e976f5935461470fbec593596fb08f5
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:48:24 +0200

    Move state hist key and component ids out of base

--- rt/state/base.js
+++ rt/state/base.js
@@ -1,16 +1,11 @@
-// state base 1.1.1
+// state base 1.1.2
 var SCRIPT_NAME = "state";
 
 var KEY_TEL_M = "ftx.tel.m";
 var KEY_TEL_ACT = "ftx.tel.act";
 var KEY_STATE_RUN = "ftx.state.run";
-var KEY_STATE_HIST = "ftx.state.hist";
 
 var STATE_STATUS_TEXT_ID = 202;
-
-var TOTAL_POWER_ID = 201;
-var VVX_EFFICIENCY_ID = 202;
-var FAN_SPEED_AVG_ID = 203;
 
 function log(s) {
   print(String(SCRIPT_NAME) + " " + s);
