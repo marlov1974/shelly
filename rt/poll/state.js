79189ddb42fc6c7eed796d5e6576cef3a39a0c09
marlov1974 <marlov74@hotmail.com>
2026-04-25 15:58:40 +0200

    Add poll state chunk

---
+++ rt/poll/state.js
@@ -0,0 +1,56 @@
+// poll state 1.0.0
+var paSupply = 0;
+var paExtract = 0;
+var lsSupply = 0;
+var lsExtract = 0;
+
+var rpmSupplyFan = 0;
+var rpmExtractFan = 0;
+var rpmVvx = 0;
+
+var tempHouseC = 0;
+var tempOutdoorC = 0;
+var tempToHouseC = 0;
+var tempPostVvxC = 0;
+var tempToOutdoorC = 0;
+var tempBrineC = 0;
+var tempHotwaterC = 0;
+
+var rhHouse = 0;
+var ppmHouse = 0;
+
+var actSupplyOn = 0;
+var actSupplyPct = 0;
+var actSupplyW = 0;
+var actSupplyRun = 0;
+
+var actExtractOn = 0;
+var actExtractPct = 0;
+var actExtractW = 0;
+var actExtractRun = 0;
+
+var actHeatOn = 0;
+var actHeatPct = 0;
+var actHeatW = 0;
+var actHeatRun = 0;
+
+var actCoolOn = 0;
+var actCoolPct = 0;
+var actCoolW = 0;
+var actCoolRun = 0;
+
+var actVvxOn = 0;
+var actVvxW = 0;
+var actVvxRun = 0;
+
+var actDampersOn = 0;
+var actDampersRun = 0;
+
+var totalPowerW = 0;
+var vvxEfficiencyPct = 0;
+var fanSpeedAvgPct = 0;
+
+var vvxEffRaw0 = 0.0;
+var vvxEffRaw1 = 0.0;
+var vvxEffRaw2 = 0.0;
+
