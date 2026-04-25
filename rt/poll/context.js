5a02844d173622585fcd5b52df72b0b748ca70ae
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:15:27 +0200

    Clean poll context for telemetry-only poll

--- rt/poll/context.js
+++ rt/poll/context.js
@@ -1,4 +1,4 @@
-// poll context 1.0.0
+// poll context 3.0.0
 function createPollCtx() {
   return {
     supply: {
@@ -8,7 +8,6 @@ function createPollCtx() {
       fan_on: 0,
       fan_pct: 0,
       fan_w: 0,
-      fan_run: 0,
       temp_post_vvx: 0,
       temp_outdoor: 0,
       temp_to_outdoor: 0
@@ -20,7 +19,6 @@ function createPollCtx() {
       fan_on: 0,
       fan_pct: 0,
       fan_w: 0,
-      fan_run: 0,
       temp_to_house: 0,
       temp_brine: 0,
       temp_hotwater: 0
@@ -31,12 +29,9 @@ function createPollCtx() {
       temp_house: 20.0,
       rh_house: 60
     },
-    heat: { on: 0, pct: 0, w: 0, run: 0 },
-    cool: { on: 0, pct: 0, w: 0, run: 0 },
-    vvx: { on: 0, w: 0, run: 0, eff_pct: 0 },
-    dmp: { on: 0, run: 0 },
-    power: { total_w: 0 },
-    fan: { avg_pct: 0 },
-    hist: { vvx_r0: 0.0, vvx_r1: 0.0, vvx_r2: 0.0 }
+    heat: { on: 0, pct: 0, w: 0 },
+    cool: { on: 0, pct: 0, w: 0 },
+    vvx: { on: 0, w: 0 },
+    dmp: { on: 0 }
   };
 }
