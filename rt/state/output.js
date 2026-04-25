ca500537ecae800756111225fa87c3070df4a517
marlov1974 <marlov74@hotmail.com>
2026-04-26 00:14:35 +0200

    Make state output consume context

--- rt/state/output.js
+++ rt/state/output.js
@@ -1,9 +1,10 @@
-// state output 1.1.2
-function writeStateRun(run, cb) {
-  kvsSet(KEY_STATE_RUN, run, cb);
+// state output 1.2.0
+function writeStateOutput(ctx, cb) {
+  kvsSet(KEY_STATE_RUN, ctx.run || {}, cb);
 }
 
-function writeStateStatus(run, cb) {
+function writeStateStatus(ctx, cb) {
+  var run = ctx.run || {};
   var s = "ST OK SR=" + run.sup + " ER=" + run.ext + " VR=" + run.vvx + " H=" + run.heat + " C=" + run.cool;
   Shelly.call("Text.Set", { id: STATE_STATUS_TEXT_ID, value: s }, function () {
     if (cb) cb();
