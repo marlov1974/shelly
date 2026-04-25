2807b846d1b338202e2661f7f79f3e51c04c14f1
marlov1974 <marlov74@hotmail.com>
2026-04-25 23:49:26 +0200

    Move fan average component id into fan average feature

--- rt/state/feature-fan-average.js
+++ rt/state/feature-fan-average.js
@@ -1,6 +1,12 @@
-// state feature-fan-average 1.1.0
+// state feature-fan-average 1.1.2
+var FAN_SPEED_AVG_ID = 203;
+
 function calcFanAverage(telM, telAct) {
   var sup = telAct && telAct.sup ? telAct.sup : {};
   var ext = telAct && telAct.ext ? telAct.ext : {};
   return clipPct((pct(sup) + pct(ext)) / 2);
+}
+
+function writeFanSpeedAvgValue(v, cb) {
+  numberSet(FAN_SPEED_AVG_ID, v, cb);
 }
