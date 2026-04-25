2bb04f9582dcba848cdd10f4e17120d4839e7a42
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:48:29 +0200

    Move poll feature constants out of central config

--- rt/poll/config.js
+++ rt/poll/config.js
@@ -1,24 +1,4 @@
-// poll config 3.0.0
-var IP_SUPPLY_UNI = "192.168.77.20";
-var IP_EXTRACT_UNI = "192.168.77.21";
-var IP_PROCESS_UNI = "192.168.77.22";
-var IP_SUPPLY_FAN = "192.168.77.10";
-var IP_EXTRACT_FAN = "192.168.77.11";
-var IP_HEAT = "192.168.77.12";
-var IP_COOL = "192.168.77.13";
-var IP_VVX = "192.168.77.40";
-var IP_DAMPERS = "192.168.77.30";
-
-var VM_DP_ID = 100;
-var INPUT_RPM_ID = 2;
-var TEMP_A_ID = 100;
-var TEMP_B_ID = 101;
-var TEMP_C_ID = 102;
-var RH_ID = 100;
-
-var K_SUPPLY_FAN = 11.6;
-var K_EXTRACT_FAN = 12.1;
-
+// poll config 3.0.2
 var KEY_TEL_M = "ftx.tel.m";
 var KEY_TEL_ACT = "ftx.tel.act";
 
