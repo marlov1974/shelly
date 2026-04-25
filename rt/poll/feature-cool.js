de41c434271ec8669ab7f28849aa11c78c749a92
marlov1974 <marlov74@hotmail.com>
2026-04-25 22:50:45 +0200

    Move cool constants into cool feature

--- rt/poll/feature-cool.js
+++ rt/poll/feature-cool.js
@@ -1,10 +1,13 @@
-// poll feature-cool 1.0.0
+// poll feature-cool 1.0.1
+var IP_COOL = "192.168.77.13";
+var COOL_MAX_W = 50;
+
 function readCool(ctx, cb) {
   httpGetStatus(IP_COOL, function (js) {
     var x = js ? parseLight0(js) : null;
     ctx.cool.on = x ? x.on : 0;
     ctx.cool.pct = x ? clipPct(x.pct) : 0;
-    ctx.cool.w = x ? clipW(x.w, 50) : 0;
+    ctx.cool.w = x ? clipW(x.w, COOL_MAX_W) : 0;
     cb();
   });
 }
