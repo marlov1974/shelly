6a84a79da6bb672ec2c05b087f5480d840397d9c
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:32:48 +0200

    Refactor poll cool to async read and classic apply

--- rt/poll/feature-cool.js
+++ rt/poll/feature-cool.js
@@ -1,12 +1,16 @@
-// poll feature-cool 1.1.0
+// poll feature-cool 1.2.0-classic-parse
 var IP_COOL = "192.168.77.13";
 
 function readCool(ctx, cb) {
   httpGetStatus(IP_COOL, function (js) {
-    var x = js ? parseLight0(js) : null;
-    ctx.cool.on = x ? x.on : 0;
-    ctx.cool.pct = x ? normPct(x.pct) : 0;
-    ctx.cool.w = x ? normW(x.w) : 0;
+    ctx.raw.cool = js;
     cb();
   });
+}
+
+function applyCool(ctx) {
+  var x = ctx.raw.cool ? parseLight0(ctx.raw.cool) : null;
+  ctx.cool.on = x ? x.on : 0;
+  ctx.cool.pct = x ? normPct(x.pct) : 0;
+  ctx.cool.w = x ? normW(x.w) : 0;
 }
