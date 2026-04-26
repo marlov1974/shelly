b42dfc0a99ba334ae13f39a46d1d7921897b0119
marlov1974 <marlov74@hotmail.com>
2026-04-26 21:47:48 +0200

    Move weather KVS key into weather IO

--- rt/brain/io-weather.js
+++ rt/brain/io-weather.js
@@ -1,5 +1,10 @@
-// brain io-weather 2.0.0
+// brain io-weather 2.1.0
+var KEY_WEATHER_ACT = "ftx.weather.act";
+
 function readWeather(ctx, cb) {
+  ctx.weather.solar_kwh_today = 0;
+  ctx.weather.temp_now_c = 0;
+
   kvsGet(KEY_WEATHER_ACT, function (v) {
     var w = (v && typeof v === "object") ? v : {};
     ctx.weather.solar_kwh_today = i(n(w.solar_kwh_today, 0));
