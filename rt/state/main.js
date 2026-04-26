6e7429455b20e6748cbdf6549b8e6e7ccc8794b9
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:13:52 +0200

    Update state main

--- rt/state/main.js
+++ rt/state/main.js
@@ -1,4 +1,4 @@
-// state main 1.3.0-classic-calc-callback-rpc
+// state main 1.3.1
 function readInput(ctx, cb) {
   kvsGet(KEY_TEL_M, function (telM) {
     ctx.telM = telM || {};
@@ -6,12 +6,6 @@ function readInput(ctx, cb) {
       ctx.telAct = telAct || {};
       cb();
     });
-  });
-}
-
-function statePause(cb) {
-  Timer.set(250, false, function () {
-    cb();
   });
 }
 
@@ -36,28 +30,15 @@ function runState() {
   log("BOT");
 
   readInput(ctx, function () {
-    statePause(function () {
-      readVvxEfficiencyHist(function (hist) {
-        statePause(function () {
-          applyRunCalculations(ctx);
-          applyPerfCalculations(ctx, hist || {});
-
-          writeStateOutput(ctx, function () {
-            statePause(function () {
-              writePowerFeature(ctx, function () {
-                statePause(function () {
-                  writeVvxEfficiencyFeature(ctx, function () {
-                    statePause(function () {
-                      writeFanAverageFeature(ctx, function () {
-                        statePause(function () {
-                          writeStateStatus(ctx, function () {
-                            log("DON");
-                          });
-                        });
-                      });
-                    });
-                  });
-                });
+    readVvxEfficiencyHist(function (hist) {
+      applyRunCalculations(ctx);
+      applyPerfCalculations(ctx, hist || {});
+      writeStateOutput(ctx, function () {
+        writePowerFeature(ctx, function () {
+          writeVvxEfficiencyFeature(ctx, function () {
+            writeFanAverageFeature(ctx, function () {
+              writeStateStatus(ctx, function () {
+                log("DON");
               });
             });
           });
