816a2e259c5f716ebc0b98da3927c435d1c54ed8
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:35:04 +0200

    Refactor poll main to async read then classic apply

--- rt/poll/main.js
+++ rt/poll/main.js
@@ -1,25 +1,40 @@
-// poll main 3.1.5-clean
-function runPoll() {
-  var ctx = createPollCtx();
-  log("BOT");
-
+// poll main 3.2.0-classic-apply
+function readAll(ctx, cb) {
   readSupply(ctx, function () {
     readExtract(ctx, function () {
       readProcess(ctx, function () {
         readHeat(ctx, function () {
           readCool(ctx, function () {
             readVvx(ctx, function () {
-              readDampers(ctx, function () {
-                writeTelemetryM(ctx, function () {
-                  writeTelemetryAct(ctx, function () {
-                    writePollStatus(ctx, function () {
-                      log("DON");
-                    });
-                  });
-                });
-              });
+              readDampers(ctx, cb);
             });
           });
+        });
+      });
+    });
+  });
+}
+
+function applyAll(ctx) {
+  applySupply(ctx);
+  applyExtract(ctx);
+  applyProcess(ctx);
+  applyHeat(ctx);
+  applyCool(ctx);
+  applyVvx(ctx);
+  applyDampers(ctx);
+}
+
+function runPoll() {
+  var ctx = createPollCtx();
+  log("BOT");
+
+  readAll(ctx, function () {
+    applyAll(ctx);
+    writeTelemetryM(ctx, function () {
+      writeTelemetryAct(ctx, function () {
+        writePollStatus(ctx, function () {
+          log("DON");
         });
       });
     });
