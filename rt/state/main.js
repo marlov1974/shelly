d2f47ffa7abde5e9d5a32167ae0572845616e9c7
marlov1974 <marlov74@hotmail.com>
2026-04-26 12:50:12 +0200

    Add 100ms pauses between state main steps

--- rt/state/main.js
+++ rt/state/main.js
@@ -1,4 +1,4 @@
-// state main 1.2.5-clean
+// state main 1.2.6-step-delay-100ms
 function readInput(ctx, cb) {
   kvsGet(KEY_TEL_M, function (telM) {
     ctx.telM = telM || {};
@@ -9,23 +9,51 @@ function readInput(ctx, cb) {
   });
 }
 
+function statePause(cb) {
+  Timer.set(100, false, function () {
+    cb();
+  });
+}
+
 function runState() {
   var ctx = createStateCtx();
 
   log("BOT");
   readInput(ctx, function () {
-    applySupplyRun(ctx, function () {
-      applyExtractRun(ctx, function () {
-        applyVvxRun(ctx, function () {
-          applyHeatRun(ctx, function () {
-            applyCoolRun(ctx, function () {
-              applyDampersRun(ctx, function () {
-                writeStateOutput(ctx, function () {
-                  applyPowerFeature(ctx, function () {
-                    applyVvxEfficiencyFeature(ctx, function () {
-                      applyFanAverageFeature(ctx, function () {
-                        writeStateStatus(ctx, function () {
-                          log("DON");
+    statePause(function () {
+      applySupplyRun(ctx, function () {
+        statePause(function () {
+          applyExtractRun(ctx, function () {
+            statePause(function () {
+              applyVvxRun(ctx, function () {
+                statePause(function () {
+                  applyHeatRun(ctx, function () {
+                    statePause(function () {
+                      applyCoolRun(ctx, function () {
+                        statePause(function () {
+                          applyDampersRun(ctx, function () {
+                            statePause(function () {
+                              writeStateOutput(ctx, function () {
+                                statePause(function () {
+                                  applyPowerFeature(ctx, function () {
+                                    statePause(function () {
+                                      applyVvxEfficiencyFeature(ctx, function () {
+                                        statePause(function () {
+                                          applyFanAverageFeature(ctx, function () {
+                                            statePause(function () {
+                                              writeStateStatus(ctx, function () {
+                                                log("DON");
+                                              });
+                                            });
+                                          });
+                                        });
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
+                          });
                         });
                       });
                     });
