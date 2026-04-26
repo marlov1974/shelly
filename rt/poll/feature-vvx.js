09d642a0d372f60195fd39f7d756d54aa4739cb0
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:33:33 +0200

    Refactor poll VVX to async read and classic apply

--- rt/poll/feature-vvx.js
+++ rt/poll/feature-vvx.js
@@ -1,11 +1,15 @@
-// poll feature-vvx 1.1.0
+// poll feature-vvx 1.2.0-classic-parse
 var IP_VVX = "192.168.77.40";
 
 function readVvx(ctx, cb) {
   httpGetStatus(IP_VVX, function (js) {
-    var x = js ? parseSwitch0(js) : null;
-    ctx.vvx.on = x ? x.on : 0;
-    ctx.vvx.w = x ? normW(x.w) : 0;
+    ctx.raw.vvx = js;
     cb();
   });
+}
+
+function applyVvx(ctx) {
+  var x = ctx.raw.vvx ? parseSwitch0(ctx.raw.vvx) : null;
+  ctx.vvx.on = x ? x.on : 0;
+  ctx.vvx.w = x ? normW(x.w) : 0;
 }
