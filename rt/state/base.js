9299e7eed9c5779f3c8a8849a8ca7cd8f67f3b55
marlov1974 <marlov74@hotmail.com>
2026-04-26 11:43:07 +0200

    Fix state status text component id

--- rt/state/base.js
+++ rt/state/base.js
@@ -1,11 +1,11 @@
-// state base 1.2.0
+// state base 1.2.4
 var SCRIPT_NAME = "state";
 
 var KEY_TEL_M = "ftx.tel.m";
 var KEY_TEL_ACT = "ftx.tel.act";
 var KEY_STATE_RUN = "ftx.state.run";
 
-var STATE_STATUS_TEXT_ID = 202;
+var STATE_STATUS_TEXT_ID = 203;
 
 function log(s) {
   print(String(SCRIPT_NAME) + " " + s);
