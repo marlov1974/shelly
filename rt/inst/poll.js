3e932e8aaf4a4af9805a2748551e74a900230355
marlov1974 <marlov74@hotmail.com>
2026-04-26 20:50:06 +0200

    Allow inst.poll to start inst.build or legacy build

--- rt/inst/poll.js
+++ rt/inst/poll.js
@@ -1,4 +1,4 @@
-// inst.poll 1.0.0
+// inst.poll 1.0.1-build-name-fallback
 (function () {
   "use strict";
 
@@ -8,6 +8,7 @@
   var VK = "ftx.ver.";
   var JOB_KEY = "ftx.install.job";
   var BUILD_NAME = "inst.build";
+  var BUILD_NAME_LEGACY = "build";
 
   function txt(s) {
     print("inst.poll " + String(s || ""));
@@ -28,7 +29,7 @@
   function fetchJson(path, tag, cb) {
     get(path, function (body) {
       var obj = body ? jp(body) : null;
-      if (!obj) { txt("IP100 " + tag + "E"); cb(null); return; }
+      if (!obj) { txt("IP101 " + tag + "E"); cb(null); return; }
       cb(obj);
     });
   }
@@ -49,7 +50,7 @@
 
   function list(cb) {
     Shelly.call("Script.List", {}, function (res, err) {
-      if (err || !res || !res.scripts) { txt("IP100 LSE"); cb([]); return; }
+      if (err || !res || !res.scripts) { txt("IP101 LSE"); cb([]); return; }
       cb(res.scripts);
     });
   }
@@ -60,6 +61,10 @@
       if (arr[i].name === name) return arr[i];
     }
     return null;
+  }
+
+  function findBuild(arr) {
+    return findByName(arr, BUILD_NAME) || findByName(arr, BUILD_NAME_LEGACY);
   }
 
   function selfStop() {
@@ -73,9 +78,9 @@
   }
 
   function createScript(pkg, cb) {
-    txt("IP100 CR " + pkg.name);
+    txt("IP101 CR " + pkg.name);
     Shelly.call("Script.Create", { name: pkg.name }, function (res, err) {
-      if (err || !res || res.id === undefined) { txt("IP100 CRE " + pkg.name); cb(null); return; }
+      if (err || !res || res.id === undefined) { txt("IP101 CRE " + pkg.name); cb(null); return; }
       pkg.id = res.id;
       cb(pkg);
     });
@@ -108,8 +113,8 @@
     Shelly.call("Script.Stop", { id: buildId }, function () {
       Timer.set(700, false, function () {
         Shelly.call("Script.Start", { id: buildId }, function (res, err2) {
-          if (err2) { txt("IP100 STE " + buildId); selfStop(); return; }
-          txt("IP100 ST " + job.name + " #" + buildId);
+          if (err2) { txt("IP101 STE " + buildId); selfStop(); return; }
+          txt("IP101 ST " + job.name + " #" + buildId);
           selfStop();
         });
       });
@@ -118,31 +123,31 @@
 
   function writeJob(job, cb) {
     Shelly.call("KVS.Set", { key: JOB_KEY, value: job }, function (res, err) {
-      if (err) { txt("IP100 JE"); cb(0); return; }
+      if (err) { txt("IP101 JE"); cb(0); return; }
       cb(1);
     });
   }
 
   function run() {
-    txt("IP100 RN");
+    txt("IP101 RN");
     Shelly.call("Shelly.GetDeviceInfo", {}, function (info, err) {
       var sid;
       var path;
-      if (err || !info) { txt("IP100 DIE"); selfStop(); return; }
+      if (err || !info) { txt("IP101 DIE"); selfStop(); return; }
       sid = shortId(info.id || info.mac || "");
       path = "rt/devices/" + sid + ".json";
-      txt("IP100 D " + sid);
+      txt("IP101 D " + sid);
       fetchJson(path, "D", function (dev) {
-        if (!dev || !dev.packages) { txt("IP100 DE"); selfStop(); return; }
-        txt("IP100 MX");
+        if (!dev || !dev.packages) { txt("IP101 DE"); selfStop(); return; }
+        txt("IP101 MX");
         list(function (scripts) {
-          var build = findByName(scripts, BUILD_NAME);
-          if (!build || build.id === undefined) { txt("IP100 NOB"); selfStop(); return; }
+          var build = findBuild(scripts);
+          if (!build || build.id === undefined) { txt("IP101 NOB"); selfStop(); return; }
           needJob(dev, scripts, 0, function (job) {
-            if (!job) { txt("IP100 OK"); selfStop(); return; }
+            if (!job) { txt("IP101 OK"); selfStop(); return; }
             writeJob(job, function (ok) {
               if (!ok) { selfStop(); return; }
-              txt("IP100 JOB " + job.name + " " + job.version + " #" + job.id);
+              txt("IP101 JOB " + job.name + " " + job.version + " #" + job.id);
               startBuild(build.id, job);
             });
           });
