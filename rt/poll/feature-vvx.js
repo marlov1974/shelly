8ee243d1046bffabc1835b80d0e9690d7d53e366
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:59:44 +0200

    Restore low-memory direct parse for poll VVX

--- rt/poll/feature-vvx.js
+++ rt/poll/feature-vvx.js
@@ -1,15 +1,15 @@
-// poll feature-vvx 1.2.0-classic-parse
+// poll feature-vvx 1.2.1-direct-parse-low-memory
 var IP_VVX = "192.168.77.40";
+
+function applyVvx(ctx, js) {
+  var x = js ? parseSwitch0(js) : null;
+  ctx.vvx.on = x ? x.on : 0;
+  ctx.vvx.w = x ? normW(x.w) : 0;
+}
 
 function readVvx(ctx, cb) {
   httpGetStatus(IP_VVX, function (js) {
-    ctx.raw.vvx = js;
+    applyVvx(ctx, js);
     cb();
   });
-}
-
-function applyVvx(ctx) {
-  var x = ctx.raw.vvx ? parseSwitch0(ctx.raw.vvx) : null;
-  ctx.vvx.on = x ? x.on : 0;
-  ctx.vvx.w = x ? normW(x.w) : 0;
 }
