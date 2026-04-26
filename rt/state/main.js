ced44521b4a7fdfce7018370bb01e23d2233644b
marlov1974 <marlov74@hotmail.com>
2026-04-26 12:56:32 +0200

    Restore state debug checkpoints with 100ms delay

--- rt/state/main.js
+++ rt/state/main.js
@@ -1,4 +1,10 @@
-// state main 1.2.6-step-delay-100ms
+// state main 1.2.7-debug-checkpoints-100ms
+function stateDbg(s, cb) {
+  Shelly.call("Text.Set", { id: STATE_STATUS_TEXT_ID, value: String(s || "") }, function () {
+    if (cb) cb();
+  });
+}
+
 function readInput(ctx, cb) {
   kvsGet(KEY_TEL_M, function (telM) {
     ctx.telM = telM || {};
@@ -15,43 +21,57 @@ function statePause(cb) {
   });
 }
 
+function stateStep(tag, fn, cb) {
+  stateDbg(tag, function () {
+    statePause(function () {
+      fn(cb);
+    });
+  });
+}
+
 function runState() {
   var ctx = createStateCtx();
 
   log("BOT");
-  readInput(ctx, function () {
-    statePause(function () {
-      applySupplyRun(ctx, function () {
-        statePause(function () {
-          applyExtractRun(ctx, function () {
-            statePause(function () {
-              applyVvxRun(ctx, function () {
-                statePause(function () {
-                  applyHeatRun(ctx, function () {
-                    statePause(function () {
-                      applyCoolRun(ctx, function () {
-                        statePause(function () {
-                          applyDampersRun(ctx, function () {
-                            statePause(function () {
-                              writeStateOutput(ctx, function () {
-                                statePause(function () {
-                                  applyPowerFeature(ctx, function () {
-                                    statePause(function () {
-                                      applyVvxEfficiencyFeature(ctx, function () {
-                                        statePause(function () {
-                                          applyFanAverageFeature(ctx, function () {
-                                            statePause(function () {
-                                              writeStateStatus(ctx, function () {
-                                                log("DON");
-                                              });
-                                            });
-                                          });
-                                        });
-                                      });
-                                    });
-                                  });
-                                });
-                              });
+  stateDbg("S D0 BOT", function () {
+    stateStep("S D1 IN", function (next) {
+      readInput(ctx, next);
+    }, function () {
+      stateStep("S D2 SR", function (next) {
+        applySupplyRun(ctx, next);
+      }, function () {
+        stateStep("S D3 ER", function (next) {
+          applyExtractRun(ctx, next);
+        }, function () {
+          stateStep("S D4 VR", function (next) {
+            applyVvxRun(ctx, next);
+          }, function () {
+            stateStep("S D5 HR", function (next) {
+              applyHeatRun(ctx, next);
+            }, function () {
+              stateStep("S D6 CR", function (next) {
+                applyCoolRun(ctx, next);
+              }, function () {
+                stateStep("S D7 DR", function (next) {
+                  applyDampersRun(ctx, next);
+                }, function () {
+                  stateStep("S D8 WO", function (next) {
+                    writeStateOutput(ctx, next);
+                  }, function () {
+                    stateStep("S D9 PWR", function (next) {
+                      applyPowerFeature(ctx, next);
+                    }, function () {
+                      stateStep("S D10 EFF", function (next) {
+                        applyVvxEfficiencyFeature(ctx, next);
+                      }, function () {
+                        stateStep("S D11 FAN", function (next) {
+                          applyFanAverageFeature(ctx, next);
+                        }, function () {
+                          stateStep("S D12 STA", function (next) {
+                            writeStateStatus(ctx, next);
+                          }, function () {
+                            stateDbg("S D13 DON", function () {
+                              log("DON");
                             });
                           });
                         });
