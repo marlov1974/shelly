7fd08adf9e773277a9c69f54cba63472756dadf3
marlov1974 <marlov74@hotmail.com>
2026-04-26 16:54:19 +0200

    Make air run functions classic synchronous state functions

--- rt/state/run-air.js
+++ rt/state/run-air.js
@@ -1,4 +1,4 @@
-// state run-air 1.2.2
+// state run-air 1.3.0-classic
 var SUPPLY_RUN_PCT_MIN = 10;
 var SUPPLY_RUN_RPM_MIN = 250;
 var SUPPLY_RUN_PA_MIN = 5;
@@ -6,22 +6,20 @@ var EXTRACT_RUN_PCT_MIN = 10;
 var EXTRACT_RUN_RPM_MIN = 250;
 var EXTRACT_RUN_PA_MIN = 5;
 
-function applySupplyRun(ctx, cb) {
+function applySupplyRun(ctx) {
   var m = ctx.telM || {};
   var a = ctx.telAct || {};
   var sup = a.sup || {};
   var rpm = m.rpm || {};
   var pa = m.pa || {};
   ctx.run.sup = b(on(sup) && pct(sup) > SUPPLY_RUN_PCT_MIN && sget(rpm, "sup", 0) > SUPPLY_RUN_RPM_MIN && sget(pa, "sup", 0) >= SUPPLY_RUN_PA_MIN);
-  cb();
 }
 
-function applyExtractRun(ctx, cb) {
+function applyExtractRun(ctx) {
   var m = ctx.telM || {};
   var a = ctx.telAct || {};
   var ext = a.ext || {};
   var rpm = m.rpm || {};
   var pa = m.pa || {};
   ctx.run.ext = b(on(ext) && pct(ext) > EXTRACT_RUN_PCT_MIN && sget(rpm, "ext", 0) > EXTRACT_RUN_RPM_MIN && sget(pa, "ext", 0) >= EXTRACT_RUN_PA_MIN);
-  cb();
 }
