72d680712818974514bf7f7ba759646802388a52
marlov1974 <marlov74@hotmail.com>
2026-04-26 16:11:30 +0200

    Restore 250ms yield after every state main step

--- rt/state/main.js
+++ rt/state/main.js
@@ -1,4 +1,4 @@
-// state main 1.2.9-rpc-only-250ms-yield
+// state main 1.2.10-clean-250ms-every-main-step
 function readInput(ctx, cb) {
   kvsGet(KEY_TEL_M, function (telM) {
     ctx.telM = telM || {};
@@ -15,23 +15,9 @@ function statePause(cb) {
   });
 }
 
-function stateRpcStep(fn, cb) {
+function stateStep(fn, cb) {
   fn(function () {
     statePause(cb);
-  });
-}
-
-function applyRunCalculations(ctx, cb) {
-  applySupplyRun(ctx, function () {
-    applyExtractRun(ctx, function () {
-      applyVvxRun(ctx, function () {
-        applyHeatRun(ctx, function () {
-          applyCoolRun(ctx, function () {
-            applyDampersRun(ctx, cb);
-          });
-        });
-      });
-    });
   });
 }
 
@@ -40,24 +26,46 @@ function runState() {
 
   log("BOT");
 
-  stateRpcStep(function (next) {
+  stateStep(function (next) {
     readInput(ctx, next);
   }, function () {
-    applyRunCalculations(ctx, function () {
-      stateRpcStep(function (next) {
-        writeStateOutput(ctx, next);
+    stateStep(function (next) {
+      applySupplyRun(ctx, next);
+    }, function () {
+      stateStep(function (next) {
+        applyExtractRun(ctx, next);
       }, function () {
-        stateRpcStep(function (next) {
-          applyPowerFeature(ctx, next);
+        stateStep(function (next) {
+          applyVvxRun(ctx, next);
         }, function () {
-          stateRpcStep(function (next) {
-            applyVvxEfficiencyFeature(ctx, next);
+          stateStep(function (next) {
+            applyHeatRun(ctx, next);
           }, function () {
-            stateRpcStep(function (next) {
-              applyFanAverageFeature(ctx, next);
+            stateStep(function (next) {
+              applyCoolRun(ctx, next);
             }, function () {
-              writeStateStatus(ctx, function () {
-                log("DON");
+              stateStep(function (next) {
+                applyDampersRun(ctx, next);
+              }, function () {
+                stateStep(function (next) {
+                  writeStateOutput(ctx, next);
+                }, function () {
+                  stateStep(function (next) {
+                    applyPowerFeature(ctx, next);
+                  }, function () {
+                    stateStep(function (next) {
+                      applyVvxEfficiencyFeature(ctx, next);
+                    }, function () {
+                      stateStep(function (next) {
+                        applyFanAverageFeature(ctx, next);
+                      }, function () {
+                        writeStateStatus(ctx, function () {
+                          log("DON");
+                        });
+                      });
+                    });
+                  });
+                });
               });
             });
           });
