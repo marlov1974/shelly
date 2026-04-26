1a130488dc31b8c9c790e873db6a8432c24c7540
marlov1974 <marlov74@hotmail.com>
2026-04-26 10:27:42 +0200

    Add detailed supply checkpoints

--- rt/poll/feature-supply.js
+++ rt/poll/feature-supply.js
@@ -1,4 +1,4 @@
-// poll feature-supply 3.1.0
+// poll feature-supply 3.1.3-debug-checkpoints
 var IP_SUPPLY_UNI = "192.168.77.20";
 var IP_SUPPLY_FAN = "192.168.77.10";
 
@@ -9,6 +9,12 @@ var TEMP_OUTDOOR_ID = 101;
 var TEMP_TO_OUTDOOR_ID = 102;
 
 var K_SUPPLY_FAN = 11.6;
+
+function supplyDbg(s, cb) {
+  Shelly.call("Text.Set", { id: POLL_STATUS_TEXT_ID, value: String(s || "") }, function () {
+    if (cb) cb();
+  });
+}
 
 function supplyPaToLs(pa) {
   if (pa <= 0) return 0;
@@ -39,21 +45,32 @@ function deriveSupplyTelemetry(ctx) {
 }
 
 function readSupply(ctx, cb) {
-  httpGetStatus(IP_SUPPLY_UNI, function (js) {
-    var x = js ? parseSupplyUni(js) : null;
-    ctx.supply.pa = x ? x.pa : 0;
-    ctx.supply.rpm = x ? x.rpm : 0;
-    ctx.supply.temp_post_vvx = x ? x.temp_post_vvx : 0;
-    ctx.supply.temp_outdoor = x ? x.temp_outdoor : 0;
-    ctx.supply.temp_to_outdoor = x ? x.temp_to_outdoor : 0;
+  supplyDbg("P S0 UNI", function () {
+    httpGetStatus(IP_SUPPLY_UNI, function (js) {
+      var x = js ? parseSupplyUni(js) : null;
+      ctx.supply.pa = x ? x.pa : 0;
+      ctx.supply.rpm = x ? x.rpm : 0;
+      ctx.supply.temp_post_vvx = x ? x.temp_post_vvx : 0;
+      ctx.supply.temp_outdoor = x ? x.temp_outdoor : 0;
+      ctx.supply.temp_to_outdoor = x ? x.temp_to_outdoor : 0;
 
-    httpGetStatus(IP_SUPPLY_FAN, function (js2) {
-      var y = js2 ? parseLight0(js2) : null;
-      ctx.supply.fan_on = y ? y.on : 0;
-      ctx.supply.fan_pct = y ? y.pct : 0;
-      ctx.supply.fan_w = y ? y.w : 0;
-      deriveSupplyTelemetry(ctx);
-      cb();
+      supplyDbg("P S1 UNI_OK", function () {
+        supplyDbg("P S2 FAN", function () {
+          httpGetStatus(IP_SUPPLY_FAN, function (js2) {
+            var y = js2 ? parseLight0(js2) : null;
+            ctx.supply.fan_on = y ? y.on : 0;
+            ctx.supply.fan_pct = y ? y.pct : 0;
+            ctx.supply.fan_w = y ? y.w : 0;
+
+            supplyDbg("P S3 FAN_OK", function () {
+              deriveSupplyTelemetry(ctx);
+              supplyDbg("P S4 DER", function () {
+                cb();
+              });
+            });
+          });
+        });
+      });
     });
   });
 }
