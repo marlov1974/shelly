55fc6cb8dfa8c6ba581ec10d133c6ccfbca4f834
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:31:57 +0200

    Refactor poll heat to async read and classic apply

--- rt/poll/feature-heat.js
+++ rt/poll/feature-heat.js
@@ -1,12 +1,16 @@
-// poll feature-heat 1.1.0
+// poll feature-heat 1.2.0-classic-parse
 var IP_HEAT = "192.168.77.12";
 
 function readHeat(ctx, cb) {
   httpGetStatus(IP_HEAT, function (js) {
-    var x = js ? parseLight0(js) : null;
-    ctx.heat.on = x ? x.on : 0;
-    ctx.heat.pct = x ? normPct(x.pct) : 0;
-    ctx.heat.w = x ? normW(x.w) : 0;
+    ctx.raw.heat = js;
     cb();
   });
+}
+
+function applyHeat(ctx) {
+  var x = ctx.raw.heat ? parseLight0(ctx.raw.heat) : null;
+  ctx.heat.on = x ? x.on : 0;
+  ctx.heat.pct = x ? normPct(x.pct) : 0;
+  ctx.heat.w = x ? normW(x.w) : 0;
 }
