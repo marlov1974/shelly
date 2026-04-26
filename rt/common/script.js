197e9d1aa55248e53151cb9997349c3520193d0c
marlov1974 <marlov74@hotmail.com>
2026-04-26 21:43:19 +0200

    Create common script helpers

---
+++ rt/common/script.js
@@ -0,0 +1,34 @@
+// common script 1.0.0
+function log(s) {
+  print(String(SCRIPT_NAME || "script") + " " + String(s || ""));
+}
+
+function findScriptByName(arr, name) {
+  var i;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].name === name) return arr[i];
+  }
+  return null;
+}
+
+function scriptStopByName(name, cb) {
+  Shelly.call("Script.List", {}, function (res, err) {
+    var s;
+    if (err || !res || !res.scripts) {
+      if (cb) cb(0);
+      return;
+    }
+    s = findScriptByName(res.scripts, name);
+    if (!s || s.id === undefined) {
+      if (cb) cb(0);
+      return;
+    }
+    Shelly.call("Script.Stop", { id: s.id }, function () {
+      if (cb) cb(1);
+    });
+  });
+}
+
+function selfStop() {
+  scriptStopByName(SCRIPT_NAME, function () {});
+}
