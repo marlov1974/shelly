d113b2d1bf76f8b62a99f5b32ff628bdde065ad3
marlov1974 <marlov74@hotmail.com>
2026-04-26 11:43:43 +0200

    Add state checkpoints and use state status component

--- rt/state/main.js
+++ rt/state/main.js
@@ -1,4 +1,10 @@
-// state main 1.2.0
+// state main 1.2.4-debug-checkpoints
+function stateDbg(s, cb) {
+  Shelly.call("Text.Set", { id: STATE_STATUS_TEXT_ID, value: String(s || "") }, function () {
+    if (cb) cb();
+  });
+}
+
 function readInput(ctx, cb) {
   kvsGet(KEY_TEL_M, function (telM) {
     ctx.telM = telM || {};
@@ -13,19 +19,43 @@ function runState() {
   var ctx = createStateCtx();
 
   log("BOT");
-  readInput(ctx, function () {
-    applySupplyRun(ctx, function () {
-      applyExtractRun(ctx, function () {
-        applyVvxRun(ctx, function () {
-          applyHeatRun(ctx, function () {
-            applyCoolRun(ctx, function () {
-              applyDampersRun(ctx, function () {
-                writeStateOutput(ctx, function () {
-                  applyPowerFeature(ctx, function () {
-                    applyVvxEfficiencyFeature(ctx, function () {
-                      applyFanAverageFeature(ctx, function () {
-                        writeStateStatus(ctx, function () {
-                          log("DON");
+  stateDbg("S D0 BOT", function () {
+    stateDbg("S D1 IN", function () {
+      readInput(ctx, function () {
+        stateDbg("S D2 SR", function () {
+          applySupplyRun(ctx, function () {
+            stateDbg("S D3 ER", function () {
+              applyExtractRun(ctx, function () {
+                stateDbg("S D4 VR", function () {
+                  applyVvxRun(ctx, function () {
+                    stateDbg("S D5 HR", function () {
+                      applyHeatRun(ctx, function () {
+                        stateDbg("S D6 CR", function () {
+                          applyCoolRun(ctx, function () {
+                            stateDbg("S D7 DR", function () {
+                              applyDampersRun(ctx, function () {
+                                stateDbg("S D8 WO", function () {
+                                  writeStateOutput(ctx, function () {
+                                    stateDbg("S D9 PWR", function () {
+                                      applyPowerFeature(ctx, function () {
+                                        stateDbg("S D10 EFF", function () {
+                                          applyVvxEfficiencyFeature(ctx, function () {
+                                            stateDbg("S D11 FAN", function () {
+                                              applyFanAverageFeature(ctx, function () {
+                                                writeStateStatus(ctx, function () {
+                                                  log("DON");
+                                                });
+                                              });
+                                            });
+                                          });
+                                        });
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
+                          });
                         });
                       });
                     });
