05ae6a202ece25b5faf48ced0a9a82c4e0e23e56
marlov1974 <marlov74@hotmail.com>
2026-04-26 10:17:47 +0200

    Add poll main checkpoints for runtime debug

--- rt/poll/main.js
+++ rt/poll/main.js
@@ -1,19 +1,45 @@
-// poll main 3.0.1
+// poll main 3.1.2-debug-checkpoints
+function pollDbg(s, cb) {
+  Shelly.call("Text.Set", { id: POLL_STATUS_TEXT_ID, value: String(s || "") }, function () {
+    if (cb) cb();
+  });
+}
+
 function runPoll() {
   var ctx = createPollCtx();
   log("BOT");
 
-  readSupply(ctx, function () {
-    readExtract(ctx, function () {
-      readProcess(ctx, function () {
-        readHeat(ctx, function () {
-          readCool(ctx, function () {
-            readVvx(ctx, function () {
-              readDampers(ctx, function () {
-                writeTelemetryM(ctx, function () {
-                  writeTelemetryAct(ctx, function () {
-                    writePollStatus(ctx, function () {
-                      log("DON");
+  pollDbg("P D0 BOT", function () {
+    pollDbg("P D1 SUP", function () {
+      readSupply(ctx, function () {
+        pollDbg("P D2 EXT", function () {
+          readExtract(ctx, function () {
+            pollDbg("P D3 PROC", function () {
+              readProcess(ctx, function () {
+                pollDbg("P D4 HEAT", function () {
+                  readHeat(ctx, function () {
+                    pollDbg("P D5 COOL", function () {
+                      readCool(ctx, function () {
+                        pollDbg("P D6 VVX", function () {
+                          readVvx(ctx, function () {
+                            pollDbg("P D7 DMP", function () {
+                              readDampers(ctx, function () {
+                                pollDbg("P D8 WTM", function () {
+                                  writeTelemetryM(ctx, function () {
+                                    pollDbg("P D9 WTA", function () {
+                                      writeTelemetryAct(ctx, function () {
+                                        writePollStatus(ctx, function () {
+                                          log("DON");
+                                        });
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
                     });
                   });
                 });
