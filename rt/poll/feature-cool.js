df50a3f5e12c84a1a3b4cae2bc6caacfecea990d
marlov1974 <marlov74@hotmail.com>
2026-04-26 17:58:40 +0200

    Restore low-memory direct parse for poll cool

--- rt/poll/feature-cool.js
+++ rt/poll/feature-cool.js
@@ -1,16 +1,16 @@
-// poll feature-cool 1.2.0-classic-parse
+// poll feature-cool 1.2.1-direct-parse-low-memory
 var IP_COOL = "192.168.77.13";
 
-function readCool(ctx, cb) {
-  httpGetStatus(IP_COOL, function (js) {
-    ctx.raw.cool = js;
-    cb();
-  });
-}
-
-function applyCool(ctx) {
-  var x = ctx.raw.cool ? parseLight0(ctx.raw.cool) : null;
+function applyCool(ctx, js) {
+  var x = js ? parseLight0(js) : null;
   ctx.cool.on = x ? x.on : 0;
   ctx.cool.pct = x ? normPct(x.pct) : 0;
   ctx.cool.w = x ? normW(x.w) : 0;
+}
+
+function readCool(ctx, cb) {
+  httpGetStatus(IP_COOL, function (js) {
+    applyCool(ctx, js);
+    cb();
+  });
 }
