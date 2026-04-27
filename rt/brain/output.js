c09c99ffaed3e1c11de30a341dc28a0499bcb0c4
marlov1974 <marlov74@hotmail.com>
2026-04-27 21:35:52 +0200

    Write target from signal bus

--- rt/brain/output.js
+++ rt/brain/output.js
@@ -1,4 +1,4 @@
-// brain output 2.1.0
+// brain output 2.2.0-signal
 var KEY_INTENT_ACT = "ftx.intent.act";
 var VM_TARGET_TO_HOUSE_ID = 204;
 
@@ -7,6 +7,6 @@ function writeIntent(ctx, cb) {
 }
 
 function writeTargetToHouse(ctx, cb) {
-  var value = d1(ctx.dx ? ctx.dx.targetToHouseC : 0);
+  var value = d1(ctx.sig ? ctx.sig.target_to_house_c : 0);
   numberSet(VM_TARGET_TO_HOUSE_ID, value, cb);
 }
