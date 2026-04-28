9324a0ddcb316e797e5df3c4519701e6cdf128b2
marlov1974 <marlov74@hotmail.com>
2026-04-28 08:27:22 +0200

    Use common logging and script helpers in state base

--- rt/state/base.js
+++ rt/state/base.js
@@ -1,46 +1,15 @@
-// state base 1.3.2-self-stop
+// state base 1.4.0-common-log
 var SCRIPT_NAME = "state";
 
 var KEY_TEL_M = "ftx.tel.m";
 var KEY_TEL_ACT = "ftx.tel.act";
 var KEY_STATE_RUN = "ftx.state.run";
 
-var STATE_STATUS_TEXT_ID = 203;
-
-function log(s) {
-  print(String(SCRIPT_NAME) + " " + s);
-}
-
-function findScriptByName(arr, name) {
-  var i;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i].name === name) return arr[i];
-  }
-  return null;
-}
-
-function selfStop() {
-  Shelly.call("Script.List", {}, function (res, err) {
-    var s;
-    if (err || !res || !res.scripts) return;
-    s = findScriptByName(res.scripts, SCRIPT_NAME);
-    if (!s || s.id === undefined) return;
-    Shelly.call("Script.Stop", { id: s.id }, function () {});
-  });
-}
-
 function createStateCtx() {
   return {
     telM: {},
     telAct: {},
-    run: {
-      sup: 0,
-      ext: 0,
-      vvx: 0,
-      heat: 0,
-      cool: 0,
-      dmp: 0
-    },
+    run: { sup: 0, ext: 0, vvx: 0, heat: 0, cool: 0, dmp: 0 },
     power_w: 0,
     fan_avg_pct: 0,
     vvx_eff_pct: 0,
