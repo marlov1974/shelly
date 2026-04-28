b51d4e451bb8d887c73517a15f58f3b9fae348f1
marlov1974 <marlov74@hotmail.com>
2026-04-28 11:35:25 +0200

    Replace weather main with one-shot print-only runtime

--- rt/weather/main.js
+++ rt/weather/main.js
@@ -1,65 +1,19 @@
-// weather main 1.0.1
-var STATUS_TEXT_ID = 200;
+// weather main 1.0.0-print-only-self-stop
+function runWeather() {
+  var ctx = createWeatherCtx();
 
-function createCtx() {
-  return { out: {} };
-}
+  log("BOT");
+  buildWeatherUrls(ctx);
+  log("DATE " + ctx.today);
 
-function setStatusText(s, cb) {
-  Shelly.call("Text.Set", { id: STATUS_TEXT_ID, value: String(s || "") }, function () {
-    if (cb) cb();
-  });
-}
-
-function saveWeatherAct(ctx, cb) {
-  log("KVS SET weather");
-  kvsWriteObject(KEY_WEATHER_ACT, ctx.out, function (ok) {
-    if (!ok) {
-      cb(0);
-      return;
-    }
-    log("KVS OK");
-    cb(1);
-  });
-}
-
-function writeStatus(ctx, cb) {
-  var s = "W OK T=" + ctx.out.temp_now + " S=" + ctx.out.solar_kwh_today;
-  setStatusText(s, cb);
-}
-
-function runOnce() {
-  var ctx = createCtx();
-  log("RUN BOT");
-
-  featureDailySolar(ctx, function () {
-    featureHourlyTemp(ctx, function () {
+  fetchDailySolar(ctx, function () {
+    fetchHourlyTemp(ctx, function () {
       saveWeatherAct(ctx, function () {
-        writeStatus(ctx, function () {
-          log("RUN DON");
-          scheduleNextRun();
-        });
+        log("DON");
+        selfStop();
       });
     });
   });
 }
 
-function scheduleNextRun() {
-  if (weatherTimer) {
-    Timer.clear(weatherTimer);
-    weatherTimer = null;
-  }
-
-  weatherTimer = Timer.set(WEATHER_PERIOD_MS, false, function () {
-    weatherTimer = null;
-    runOnce();
-  });
-}
-
-function boot() {
-  log("BOOT");
-  runOnce();
-}
-
-boot();
-
+runWeather();
