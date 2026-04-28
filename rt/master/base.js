1b8080ccf985516b5f83dcd23d5bd56fb5912cd5
marlov1974 <marlov74@hotmail.com>
2026-04-28 20:58:06 +0200

    Create master base

---
+++ rt/master/base.js
@@ -0,0 +1,15 @@
+// master base 1.0.0
+var SCRIPT_NAME = "master";
+
+var INSTALLER_ID = 1;
+var TICK_MS = 60000;
+var WEATHER_EVERY_TICKS = 30;
+var INSTALL_EVERY_TICKS = 5;
+
+var TIMEOUT_POLL_MS = 25000;
+var TIMEOUT_STATE_MS = 10000;
+var TIMEOUT_WEATHER_MS = 30000;
+var TIMEOUT_BRAIN_MS = 10000;
+
+var tickCount = 0;
+var cycleRunning = 0;
