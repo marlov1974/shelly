700dfd5266fa879f32cd0353a14eae1357b1b4a7
marlov1974 <marlov74@hotmail.com>
2026-04-28 21:01:18 +0200

    Prefer versioned script names in master runner

--- rt/master/run.js
+++ rt/master/run.js
@@ -1,12 +1,30 @@
-// master run 1.0.0
+// master run 1.0.1-prefer-versioned
 function findByRolePrefix(scripts, role) {
   var i;
   var prefix = role + "_v";
   for (i = 0; i < scripts.length; i++) {
     if (String(scripts[i].name || "").indexOf(prefix) === 0) return scripts[i];
+  }
+  for (i = 0; i < scripts.length; i++) {
     if (scripts[i].name === role) return scripts[i];
   }
   return null;
+}
+
+function waitUntilStopped(id, cb) {
+  Timer.set(500, false, function () {
+    Shelly.call("Script.List", {}, function (res, err) {
+      var i;
+      var s = null;
+      if (err || !res || !res.scripts) { cb(0); return; }
+      for (i = 0; i < res.scripts.length; i++) {
+        if (res.scripts[i].id === id) { s = res.scripts[i]; break; }
+      }
+      if (!s) { cb(0); return; }
+      if (!s.running) { cb(1); return; }
+      waitUntilStopped(id, cb);
+    });
+  });
 }
 
 function startScriptByRole(role, timeoutMs, cb) {
@@ -22,49 +40,18 @@ function startScriptByRole(role, timeoutMs, cb) {
       cb(ok);
     }
 
-    if (err || !res || !res.scripts) {
-      log("LS ERR " + role);
-      cb(0);
-      return;
-    }
-
+    if (err || !res || !res.scripts) { log("LS ERR " + role); cb(0); return; }
     s = findByRolePrefix(res.scripts, role);
-    if (!s || s.id === undefined) {
-      log("NO " + role);
-      cb(0);
-      return;
-    }
+    if (!s || s.id === undefined) { log("NO " + role); cb(0); return; }
 
     log("ST " + role + " #" + s.id);
     Shelly.call("Script.Start", { id: s.id }, function (r, e) {
-      if (e) {
-        log("STE " + role);
-        finish(0);
-        return;
-      }
-
+      if (e) { log("STE " + role); finish(0); return; }
       timer = Timer.set(timeoutMs, false, function () {
         log("TO " + role);
         Shelly.call("Script.Stop", { id: s.id }, function () { finish(0); });
       });
-
       waitUntilStopped(s.id, function (ok) { finish(ok); });
-    });
-  });
-}
-
-function waitUntilStopped(id, cb) {
-  Timer.set(500, false, function () {
-    Shelly.call("Script.List", {}, function (res, err) {
-      var i;
-      var s;
-      if (err || !res || !res.scripts) { cb(0); return; }
-      for (i = 0; i < res.scripts.length; i++) {
-        if (res.scripts[i].id === id) { s = res.scripts[i]; break; }
-      }
-      if (!s) { cb(0); return; }
-      if (!s.running) { cb(1); return; }
-      waitUntilStopped(id, cb);
     });
   });
 }
