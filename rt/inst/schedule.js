5c380b8b66fe8c0661fd140122459a2372657fd3
marlov1974 <marlov74@hotmail.com>
2026-04-28 08:38:28 +0200

    Make installer scheduler print-only

--- rt/inst/schedule.js
+++ rt/inst/schedule.js
@@ -1,16 +1,12 @@
-// inst.schedule 1.0.0
+// inst.schedule 1.1.0-print-only
 (function () {
   "use strict";
 
   var SCRIPT_NAME = "inst.schedule";
   var POLL_NAME = "inst.poll";
-  var TEXT_ID = 201;
   var INTERVAL_MS = 300000;
 
-  function txt(s) {
-    print("inst.schedule " + String(s || ""));
-    Shelly.call("Text.Set", { id: TEXT_ID, value: String(s || "") }, function () {});
-  }
+  function log(s) { print(SCRIPT_NAME + " " + String(s || "")); }
 
   function findByName(arr, name) {
     var i;
@@ -23,12 +19,12 @@
   function startPoll(cb) {
     Shelly.call("Script.List", {}, function (res, err) {
       var s;
-      if (err || !res || !res.scripts) { txt("IS100 LSE"); if (cb) cb(); return; }
+      if (err || !res || !res.scripts) { log("IS110 LSE"); if (cb) cb(); return; }
       s = findByName(res.scripts, POLL_NAME);
-      if (!s || s.id === undefined) { txt("IS100 NOP"); if (cb) cb(); return; }
+      if (!s || s.id === undefined) { log("IS110 NOP"); if (cb) cb(); return; }
       Shelly.call("Script.Start", { id: s.id }, function (r, e) {
-        if (e) txt("IS100 STE " + s.id);
-        else txt("IS100 ST " + s.id);
+        if (e) log("IS110 STE " + s.id);
+        else log("IS110 ST " + s.id);
         if (cb) cb();
       });
     });
@@ -40,6 +36,6 @@
     });
   }
 
-  txt("IS100 RN");
+  log("IS110 RN");
   loop();
 })();
