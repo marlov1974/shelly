774ced3a7058ea8ad741e8014998cf3efdc278d5
marlov1974 <marlov74@hotmail.com>
2026-04-29 18:20:34 +0200

    Read run state from ftx.state.run in brain inputs

--- rt/brain/io-inputs.js
+++ rt/brain/io-inputs.js
@@ -1,6 +1,7 @@
-// brain io-inputs 2.1.0
+// brain io-inputs 2.4.1-read-state-run
 var KEY_TEL_M = "ftx.tel.m";
 var KEY_TEL_ACT = "ftx.tel.act";
+var KEY_STATE_RUN = "ftx.state.run";
 
 function getTelMValue(telM, path1, path2, def) {
   var a = telM && telM[path1];
@@ -20,7 +21,12 @@ function getTelActBoolValue(telAct, dev, field, def) {
   return b(a[field]);
 }
 
-function applyInputs(ctx, telM, telAct) {
+function getRunValue(run, field, def) {
+  if (!run || typeof run !== "object") return def;
+  return b(run[field]);
+}
+
+function applyInputs(ctx, telM, telAct, stateRun) {
   ctx.inp.t_out_c = getTelMValue(telM, "t", "out", ctx.cmd.house_temp_c);
   ctx.inp.t_house_c = getTelMValue(telM, "t", "house", ctx.cmd.house_temp_c);
   ctx.inp.t_to_house_c = getTelMValue(telM, "t", "to_house", ctx.cmd.house_temp_c);
@@ -28,9 +34,9 @@ function applyInputs(ctx, telM, telAct) {
   ctx.inp.rh_house_pct = getTelMValue(telM, "rh", "house", 50.0);
   ctx.inp.ppm_house = getTelMValue(telM, "ppm", "house", 500.0);
 
-  ctx.inp.dmp_run = getTelActBoolValue(telAct, "dmp", "run", 0);
-  ctx.inp.sup_run = getTelActBoolValue(telAct, "sup", "run", 0);
-  ctx.inp.ext_run = getTelActBoolValue(telAct, "ext", "run", 0);
+  ctx.inp.dmp_run = getRunValue(stateRun, "dmp", getTelActBoolValue(telAct, "dmp", "run", 0));
+  ctx.inp.sup_run = getRunValue(stateRun, "sup", getTelActBoolValue(telAct, "sup", "run", 0));
+  ctx.inp.ext_run = getRunValue(stateRun, "ext", getTelActBoolValue(telAct, "ext", "run", 0));
   ctx.inp.vvx_on_actual = getTelActBoolValue(telAct, "vvx", "on", 0);
   ctx.inp.heat_pct_actual = getTelActNumValue(telAct, "heat", "pct", 0);
   ctx.inp.cool_pct_actual = getTelActNumValue(telAct, "cool", "pct", 0);
@@ -41,8 +47,11 @@ function readInputs(ctx, cb) {
     var telM = (m && typeof m === "object") ? m : {};
     kvsGet(KEY_TEL_ACT, function (a) {
       var telAct = (a && typeof a === "object") ? a : {};
-      applyInputs(ctx, telM, telAct);
-      cb();
+      kvsGet(KEY_STATE_RUN, function (r) {
+        var stateRun = (r && typeof r === "object") ? r : {};
+        applyInputs(ctx, telM, telAct, stateRun);
+        cb();
+      });
     });
   });
 }
