0ceaf5581a34bc61da1dcee5fdf4abdceac88ca4
marlov1974 <marlov74@hotmail.com>
2026-04-29 20:24:50 +0200

    Make master fixed-id low-memory base

--- rt/master/base.js
+++ rt/master/base.js
@@ -1,8 +1,13 @@
-// master base 1.2.0-slotted-60s
+// master base 1.2.1-fixed-id-low-memory
 var SCRIPT_NAME = "master";
 
 var INSTALLER_ID = 1;
 var MASTER_ID = 2;
+var POLL_ID = 3;
+var STATE_ID = 4;
+var WEATHER_ID = 5;
+var BRAIN_ID = 6;
+var DRIVER_ID = 7;
 
 var TICK_MS = 60000;
 var CLEANUP_AT_MS = 50000;
@@ -20,3 +25,6 @@ var TIMEOUT_DRIVER_MS = 15000;
 var tickCount = 0;
 var cycleRunning = 0;
 var cycleStartMs = 0;
+
+function n(v, d) { var x = Number(v); return (x === x) ? x : d; }
+function i(v) { var x = Number(v); if (x !== x) return 0; return Math.floor(x + 0.5); }
