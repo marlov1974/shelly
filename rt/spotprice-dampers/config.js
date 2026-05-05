31c8eab0d8f9bdb64f63a3fe94441a366d6208f2
marlov1974 <marlov74@hotmail.com>
2026-05-05 20:07:16 +0200

    Add Tibber spotprice debug keys

--- rt/spotprice-dampers/config.js
+++ rt/spotprice-dampers/config.js
@@ -1,4 +1,4 @@
-// spotprice-dampers config 1.1.0-tibber
+// spotprice-dampers config 1.1.1-tibber-debug
 var TIBBER_TOKEN_TEXT_ID = 201;
 var TIBBER_URL = "https://api.tibber.com/v1-beta/gql";
 var FETCH_TOMORROW = true;
@@ -8,3 +8,5 @@ var KEY_PRICE_DATE = "hp.price.date";
 var KEY_PRICE_STATUS = "hp.price.status";
 var KEY_PRICE_UPDATED = "hp.price.updated";
 var KEY_PRICE_SOURCE = "hp.price.source";
+var KEY_PRICE_DEBUG = "hp.price.debug";
+var KEY_PRICE_DEBUG_LEN = "hp.price.debug.len";
