eae0b5f3ad1369e389e067aeaca1be5dac546f54
marlov1974 <marlov74@hotmail.com>
2026-05-05 21:20:14 +0200

    Add dampers weather config chunk

---
+++ rt/weather-dampers/config.js
@@ -0,0 +1,13 @@
+// weather-dampers config 1.1.0
+// Location: Jakobsberg / Stockholm area. Adjust here if the installation is moved.
+var WEATHER_LAT = "59.42";
+var WEATHER_LON = "17.84";
+var WEATHER_TZ = "Europe%2FStockholm";
+var WEATHER_URL = "https://api.open-meteo.com/v1/forecast";
+
+var KEY_WEATHER_STATUS = "hp.weather.status";
+var KEY_WEATHER_TEMP_C = "hp.weather.temp.c";
+var KEY_WEATHER_SOLAR_WM2 = "hp.weather.solar.wm2";
+var KEY_WEATHER_DAY_AVG_TEMP_C = "hp.weather.temp.day_avg.c";
+var KEY_WEATHER_UPDATED = "hp.weather.updated";
+var KEY_WEATHER_SOURCE = "hp.weather.source";
