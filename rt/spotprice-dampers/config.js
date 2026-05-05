b3c32d44f7a2ea86565b240c98d90edea81a944f
marlov1974 <marlov74@hotmail.com>
2026-05-05 19:59:35 +0200

    Add dampers Tibber spotprice config chunk

---
+++ rt/spotprice-dampers/config.js
@@ -0,0 +1,10 @@
+// spotprice-dampers config 1.1.0-tibber
+var TIBBER_TOKEN_TEXT_ID = 201;
+var TIBBER_URL = "https://api.tibber.com/v1-beta/gql";
+var FETCH_TOMORROW = true;
+
+var KEY_PRICE_2H = "hp.price.2h";
+var KEY_PRICE_DATE = "hp.price.date";
+var KEY_PRICE_STATUS = "hp.price.status";
+var KEY_PRICE_UPDATED = "hp.price.updated";
+var KEY_PRICE_SOURCE = "hp.price.source";
