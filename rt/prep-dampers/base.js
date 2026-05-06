d920a0535485e002c88005020e4fafd7c21a7eec
marlov1974 <marlov74@hotmail.com>
2026-05-06 06:58:44 +0200

    Add dampers optimization prep base chunk

---
+++ rt/prep-dampers/base.js
@@ -0,0 +1,16 @@
+// prep-dampers base 1.0.0
+var SCRIPT_NAME = "prep";
+var SCRIPT_ID = 6;
+
+var COMFORT_ENUM_ID = 200;
+var TARGET_TEMP_NUMBER_ID = 200;
+
+var IP_VVX = "192.168.77.40";
+var KEY_VVX_TEL_M = "ftx.tel.m";
+var KEY_WEATHER_DAY_AVG_TEMP_C = "hp.weather.temp.day_avg.c";
+
+var KEY_PREP_STATUS = "hp.opt.prep.status";
+var KEY_PREP_OBJECT = "hp.opt.prep";
+var KEY_PREP_START_PLAN = "hp.opt.start_plan";
+var KEY_PREP_REQUIRED_HEAT_KWH = "hp.opt.required_heat_kwh";
+var KEY_PREP_UPDATED = "hp.opt.prep.updated";
