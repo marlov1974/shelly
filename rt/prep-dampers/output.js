40c40079869901fab2fad5e84ef21382786f3961
marlov1974 <marlov74@hotmail.com>
2026-05-06 23:25:50 +0200

    Write only single prep output KVS

--- rt/prep-dampers/output.js
+++ rt/prep-dampers/output.js
@@ -1,15 +1,7 @@
-// prep-dampers output 1.0.0
+// prep-dampers output 1.1.0-one-output
 function writePrep(p, cb) {
   kvsSet(KEY_PREP_OBJECT, p, function () {
-    kvsSet(KEY_PREP_START_PLAN, p.start_plan, function () {
-      kvsSet(KEY_PREP_REQUIRED_HEAT_KWH, String(p.required_heat_kwh), function () {
-        kvsSet(KEY_PREP_UPDATED, p.updated, function () {
-          kvsSet(KEY_PREP_STATUS, "ok", function () {
-            log("OK req=" + p.required_heat_kwh + " plan=" + p.start_plan);
-            if (cb) cb();
-          });
-        });
-      });
-    });
+    log("OK req=" + p.required_heat_kwh + " plan=" + p.start_plan);
+    if (cb) cb();
   });
 }
