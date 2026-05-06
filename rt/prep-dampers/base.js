ef433404f87ab374f525e81e9165c4fd43bd91e1
marlov1974 <marlov74@hotmail.com>
2026-05-06 23:31:42 +0200

    Add price input for prep level costs

--- rt/prep-dampers/base.js
+++ rt/prep-dampers/base.js
@@ -1,4 +1,4 @@
-// prep-dampers base 1.1.0-one-output
+// prep-dampers base 1.2.0-costed-levels
 var SCRIPT_NAME = "prep";
 var SCRIPT_ID = 6;
 
@@ -8,6 +8,7 @@ var TARGET_TEMP_NUMBER_ID = 200;
 var IP_VVX = "192.168.77.40";
 var KEY_VVX_TEL_M = "ftx.tel.m";
 var KEY_WEATHER_DAY_AVG_TEMP_C = "hp.weather.temp.day_avg.c";
+var KEY_PRICE_2H = "hp.price.2h";
 
 // Single output KVS for optimizer preparation.
 var KEY_PREP_OBJECT = "hp.opt.prep";
