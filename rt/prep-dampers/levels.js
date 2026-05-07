9c7283570045502de77491ea2c505d9cacd074dc
marlov1974 <marlov74@hotmail.com>
2026-05-07 14:10:03 +0200

    Split prep levels builder into smaller chunk

--- rt/prep-dampers/levels.js
+++ rt/prep-dampers/levels.js
@@ -1,61 +1,39 @@
-// prep-dampers levels 1.5.0-flat-builder
-// levels: block,level,electric_kW,heat_kWh_per_2h,cost
-// steps:  block,from,to,delta_heat_kWh,delta_cost,sek_per_kWh
-function parsePriceAt(csv, idx) {
-  var s = String(csv || "");
-  var p = 0;
-  var k = 0;
-  while (k < idx) {
-    p = s.indexOf(",", p);
-    if (p < 0) return 0;
-    p++;
-    k++;
-  }
-  var e = s.indexOf(",", p);
-  if (e < 0) e = s.length;
-  return n(s.substring(p, e), 0);
+// prep-dampers levels 1.6.0-split
+function addLevelRow(out, b, l, e, h, c) {
+  if (out !== "") out += ";";
+  return out + b + "," + l + "," + e + "," + h + "," + c;
+}
+
+function addStepRow(out, b, from, to, dh, dc) {
+  var spk = 0;
+  if (dh > 0) spk = d1(dc / dh);
+  if (out !== "") out += ";";
+  return out + b + "," + from + "," + to + "," + dh + "," + dc + "," + spk;
 }
 
 function buildLevelData(periodName, priceCsv) {
-  var start = 0;
-  var e0 = 0.8, e1 = 1.1, e2 = 1.8, e3 = 2.4, e4 = 7.5;
-  var h0 = 8.8, h1 = 11.4, h2 = 18.8, h3 = 24.0, h4 = 54.0;
+  selectLevelSet(periodName);
   var levels = "";
   var steps = "";
   var b;
-
-  if (periodName === "p2") {
-    start = 4;
-    e0 = 0.5; e1 = 0.7; e2 = 1.2; e3 = 1.7; e4 = 5.2;
-    h0 = 5.8; h1 = 7.8; h2 = 12.8; h3 = 17.0; h4 = 40.0;
-  }
-  if (periodName === "p3") {
-    start = 8;
-    e0 = 0.65; e1 = 0.9; e2 = 1.5; e3 = 2.0; e4 = 6.5;
-    h0 = 7.2; h1 = 9.6; h2 = 15.6; h3 = 20.0; h4 = 48.0;
-  }
-
   for (b = 0; b < 4; b++) {
-    var price = parsePriceAt(priceCsv, start + b);
-    var c0 = d1(price * e0 * 2.0);
-    var c1 = d1(price * e1 * 2.0);
-    var c2 = d1(price * e2 * 2.0);
-    var c3 = d1(price * e3 * 2.0);
-    var c4 = d1(price * e4 * 2.0);
+    var price = parsePriceAt(priceCsv, LV_START + b);
+    var c0 = d1(price * LV_E0 * 2.0);
+    var c1 = d1(price * LV_E1 * 2.0);
+    var c2 = d1(price * LV_E2 * 2.0);
+    var c3 = d1(price * LV_E3 * 2.0);
+    var c4 = d1(price * LV_E4 * 2.0);
 
-    if (levels !== "") levels += ";";
-    levels += b + ",0," + e0 + "," + h0 + "," + c0;
-    levels += ";" + b + ",1," + e1 + "," + h1 + "," + c1;
-    levels += ";" + b + ",2," + e2 + "," + h2 + "," + c2;
-    levels += ";" + b + ",3," + e3 + "," + h3 + "," + c3;
-    levels += ";" + b + ",4," + e4 + "," + h4 + "," + c4;
+    levels = addLevelRow(levels, b, 0, LV_E0, LV_H0, c0);
+    levels = addLevelRow(levels, b, 1, LV_E1, LV_H1, c1);
+    levels = addLevelRow(levels, b, 2, LV_E2, LV_H2, c2);
+    levels = addLevelRow(levels, b, 3, LV_E3, LV_H3, c3);
+    levels = addLevelRow(levels, b, 4, LV_E4, LV_H4, c4);
 
-    if (steps !== "") steps += ";";
-    steps += b + ",0,1," + d1(h1 - h0) + "," + d1(c1 - c0) + "," + d1((c1 - c0) / (h1 - h0));
-    steps += ";" + b + ",1,2," + d1(h2 - h1) + "," + d1(c2 - c1) + "," + d1((c2 - c1) / (h2 - h1));
-    steps += ";" + b + ",2,3," + d1(h3 - h2) + "," + d1(c3 - c2) + "," + d1((c3 - c2) / (h3 - h2));
-    steps += ";" + b + ",3,4," + d1(h4 - h3) + "," + d1(c4 - c3) + "," + d1((c4 - c3) / (h4 - h3));
+    steps = addStepRow(steps, b, 0, 1, d1(LV_H1 - LV_H0), d1(c1 - c0));
+    steps = addStepRow(steps, b, 1, 2, d1(LV_H2 - LV_H1), d1(c2 - c1));
+    steps = addStepRow(steps, b, 2, 3, d1(LV_H3 - LV_H2), d1(c3 - c2));
+    steps = addStepRow(steps, b, 3, 4, d1(LV_H4 - LV_H3), d1(c4 - c3));
   }
-
   return { levels: levels, steps: steps };
 }
