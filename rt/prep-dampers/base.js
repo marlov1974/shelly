d758eb6b38adf948326da1ef5b854ea4675e28b1
marlov1974 <marlov74@hotmail.com>
2026-05-07 21:20:50 +0200

    Add solar input and heat model constants to prep

--- rt/prep-dampers/base.js
+++ rt/prep-dampers/base.js
@@ -1,4 +1,4 @@
-// prep-dampers base 1.2.0-costed-levels
+// prep-dampers base 1.9.0-heat-balance
 var SCRIPT_NAME = "prep";
 var SCRIPT_ID = 6;
 
@@ -8,7 +8,12 @@ var TARGET_TEMP_NUMBER_ID = 200;
 var IP_VVX = "192.168.77.40";
 var KEY_VVX_TEL_M = "ftx.tel.m";
 var KEY_WEATHER_DAY_AVG_TEMP_C = "hp.weather.temp.day_avg.c";
+var KEY_WEATHER_SOLAR_WM2 = "hp.weather.solar.wm2";
 var KEY_PRICE_2H = "hp.price.2h";
+
+var HOUSE_LOSS_KWH_DAY_PER_C = 12.5;
+var BASE_INTERNAL_KWH_DAY = 42.0;
+var SOLAR_KWH_DAY_PER_WM2 = 0.06;
 
 // Single output KVS for optimizer preparation.
 var KEY_PREP_OBJECT = "hp.opt.prep";
