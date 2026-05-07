58a0fdc139044d2cf7371480498649062106017f
marlov1974 <marlov74@hotmail.com>
2026-05-07 21:29:49 +0200

    Pass solar value into prep heat balance

--- rt/prep-dampers/main.js
+++ rt/prep-dampers/main.js
@@ -1,4 +1,4 @@
-// prep-dampers main 1.2.0-costed-levels
+// prep-dampers main 1.9.0-heat-balance
 function run() {
   var ctx = { now: new Date() };
   readComfort(function (comfort) {
@@ -14,15 +14,18 @@ function run() {
         ctx.houseTemp = houseTemp;
         readWeatherDayAvg(function (dayAvgTemp) {
           ctx.dayAvgTemp = dayAvgTemp;
-          readPriceBlocks(function (priceCsv) {
-            ctx.priceCsv = priceCsv;
-            ctx.periodName = periodName(ctx.now);
-            ctx.periodHours = periodHours(ctx.now);
-            ctx.cpHour = checkpointHour(ctx.now);
-            ctx.cpWeekday = checkpointWeekday(ctx.now);
-            ctx.targetPct = checkpointTargetPct(ctx.cpWeekday, ctx.cpHour);
-            writePrep(buildPrep(ctx), function () {
-              selfStop();
+          readSolar(function (solarWm2) {
+            ctx.solarWm2 = solarWm2;
+            readPriceBlocks(function (priceCsv) {
+              ctx.priceCsv = priceCsv;
+              ctx.periodName = periodName(ctx.now);
+              ctx.periodHours = periodHours(ctx.now);
+              ctx.cpHour = checkpointHour(ctx.now);
+              ctx.cpWeekday = checkpointWeekday(ctx.now);
+              ctx.targetPct = checkpointTargetPct(ctx.cpWeekday, ctx.cpHour);
+              writePrep(buildPrep(ctx), function () {
+                selfStop();
+              });
             });
           });
         });
