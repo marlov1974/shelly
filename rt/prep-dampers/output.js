59a23ee052d750e0924bb75fe7898d197ba68c49
marlov1974 <marlov74@hotmail.com>
2026-05-07 14:29:58 +0200

    Write compact prep string output

--- rt/prep-dampers/output.js
+++ rt/prep-dampers/output.js
@@ -1,7 +1,8 @@
-// prep-dampers output 1.1.0-one-output
+// prep-dampers output 1.7.0-compact-output
 function writePrep(p, cb) {
-  kvsSet(KEY_PREP_OBJECT, p, function () {
-    log("OK req=" + p.required_heat_kwh + " plan=" + p.start_plan);
+  kvsSet(KEY_PREP_OBJECT, p, function (ok) {
+    if (!ok) { log("WRITE ERR"); if (cb) cb(); return; }
+    log("OK " + String(p).substring(0, 80));
     if (cb) cb();
   });
 }
