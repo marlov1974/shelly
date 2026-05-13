afe7648d9478a57ae79fc2e54ad3908de158a4ef
marlov1974 <marlov74@hotmail.com>
2026-05-13 21:16:56 +0200

    Fix state base power contract

--- rt/state/perf-power.js
+++ rt/state/perf-power.js
@@ -1,26 +1,29 @@
-// state perf-power 1.6.0-primary-power-component
-var TOTAL_POWER_ID = 201;
+// state perf-power 1.6.2-base-power-contract
+var BASE_POWER_ID = 201;
 var PRIMARY_POWER_ID = 205;
-var IDLE_POWER_W = 14;
+var BASE_POWER_W = 14;
 
-function calcPower(telM, telAct) {
+function calcMeasuredPower(telAct) {
   var sup = telAct && telAct.sup ? telAct.sup : {};
   var ext = telAct && telAct.ext ? telAct.ext : {};
   var vvx = telAct && telAct.vvx ? telAct.vvx : {};
   var heat = telAct && telAct.heat ? telAct.heat : {};
   var cool = telAct && telAct.cool ? telAct.cool : {};
   var dmp = telAct && telAct.dmp ? telAct.dmp : {};
-  var total = IDLE_POWER_W + w(sup) + w(ext) + w(vvx) + w(heat) + w(cool) + w(dmp);
-  return i(clip(total, 0, 9999));
+  return i(clip(w(sup) + w(ext) + w(vvx) + w(heat) + w(cool) + w(dmp), 0, 9999));
+}
+
+function calcPower(telM, telAct) {
+  return i(clip(BASE_POWER_W + calcMeasuredPower(telAct || {}), 0, 9999));
 }
 
 function calcPowerFeature(ctx) {
+  ctx.base_power_w = BASE_POWER_W;
   ctx.power_w = calcPower(ctx.telM || {}, ctx.telAct || {});
 }
 
 function writePowerFeature(ctx, cb) {
-  var p = ctx.power_w || 0;
-  numberSet(TOTAL_POWER_ID, p, function () {
-    numberSet(PRIMARY_POWER_ID, p, cb);
+  numberSet(BASE_POWER_ID, ctx.base_power_w || BASE_POWER_W, function () {
+    numberSet(PRIMARY_POWER_ID, ctx.power_w || 0, cb);
   });
 }
