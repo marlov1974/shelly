c16007890be8a4bca62bf4578c152d9d8d5d04f9
marlov1974 <marlov74@hotmail.com>
2026-05-13 21:17:44 +0200

    Parse weather daily mean temperature

--- rt/weather/parse.js
+++ rt/weather/parse.js
@@ -1,26 +1,36 @@
-// weather parse 1.0.0
-function parseDailySolar(body) {
+// weather parse 1.1.0-daily-mean-temp
+function parseDailyWeather(body) {
   var js;
   var daily;
   var swrArr;
+  var tempAvgArr;
   var mj;
   var kwh;
+  var tempAvg;
 
-  try { js = JSON.parse(body); } catch (e) { log("JSON DAILY ERR"); return 0; }
+  try { js = JSON.parse(body); } catch (e) { log("JSON DAILY ERR"); return { solar_kwh_today: 0, temp_avg_today: 0 }; }
 
   daily = js && js.daily;
-  if (!daily || typeof daily !== "object") { log("NO DAILY"); return 0; }
+  if (!daily || typeof daily !== "object") { log("NO DAILY"); return { solar_kwh_today: 0, temp_avg_today: 0 }; }
 
   swrArr = daily.shortwave_radiation_sum;
-  if (!swrArr || !swrArr.length) { log("NO SWR"); return 0; }
-
-  mj = n(swrArr[0], 0);
+  if (!swrArr || !swrArr.length) { log("NO SWR"); mj = 0; }
+  else mj = n(swrArr[0], 0);
   if (mj < 0) mj = 0;
   kwh = i(clip(mj * SOLAR_GAIN_FACTOR_KWH_PER_MJ, 0, 999));
 
+  tempAvgArr = daily.temperature_2m_mean;
+  if (!tempAvgArr || !tempAvgArr.length) { log("NO TAVG"); tempAvg = 0; }
+  else tempAvg = d1(clip(tempAvgArr[0], -99.9, 99.9));
+
   log("SWR MJ=" + mj);
   log("SOL KWH=" + kwh);
-  return kwh;
+  log("TAVG=" + tempAvg);
+  return { solar_kwh_today: kwh, temp_avg_today: tempAvg };
+}
+
+function parseDailySolar(body) {
+  return parseDailyWeather(body).solar_kwh_today;
 }
 
 function parseHourlyTemp(body) {
